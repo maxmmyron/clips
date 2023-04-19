0dba9ab50c8c2e9eed5d078c279b7613530cf812
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-20 00:47:22 +0100

    implement dead simple uploading sys

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,22 @@
   import "../app.css";
 
   let isPlaying = false;
+
+  let files: FileList | null = null;
+  $: file = files && files[0];
 </script>
 
 <main class="w-full h-[100dvh] bg-neutral-950 grid grid-cols-1 grid-rows-[70vh,30vh]">
   <div class="p-8">
     <div class="d-flex gap-4">
-      <button class="text-white border-2 border-neutral-800 px-3 py-1">upload</button>
+      <input type="file" accept="video/*" class="text-white border-2 border-neutral-800 px-3 py-1" bind:files />
     </div>
-    <video class="aspect-video mx-auto border-2 border-neutral-800 max-h-full" />
+    <video class="aspect-video mx-auto border-2 border-neutral-800 max-h-full" controls={file !== null}>
+      {#if file}
+        <source src={URL.createObjectURL(file)} type={file.type} />
+      {/if}
+      <track kind="captions" />
+    </video>
   </div>
   <div class="m-8 border-2 border-neutral-800">
     <div class="d-flex gap-4">
