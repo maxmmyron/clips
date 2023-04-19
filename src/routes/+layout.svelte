1cd99cbce1656cb49e37bb708b6e4875e2adc2a2
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-20 00:22:11 +0100

    initial layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import "../app.css";
+</script>
+
+<main class="w-full h-[100dvh] bg-neutral-950 grid grid-cols-1 grid-rows-[70vh,30vh]">
+  <div class="p-8">
+    <video class="aspect-video mx-auto border-2 border-neutral-800 rounded-lg max-h-full" />
+  </div>
+  <div class="m-8 rounded-lg border-2 border-neutral-800" />
+</main>
