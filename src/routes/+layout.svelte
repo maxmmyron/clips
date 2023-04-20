c363651466c2e2877db2d431f7567ca32aeae45f
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-20 22:47:16 +0100

    improve layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,8 @@
   import "../app.css";
 
   let isPlaying = false;
-  let isMediaPoolVisible = false;
   let video: HTMLVideoElement | null = null;
-  // TODO: support multiple files
+
   let files: FileList | null = null;
 
   $: if (video && isPlaying) video.play();
@@ -16,41 +15,86 @@
     video.pause();
     video.currentTime = time === -1 ? video.duration : time;
   };
+
+  const handleResize = (e: MouseEvent) => {
+    if (!isResizing || !resizeMode) return;
+
+    if (resizeMode === "col") columnWidth = `${e.clientX}px`;
+    else if (resizeMode === "row") rowWidth = `calc(100vh - ${e.clientY}px)`;
+    else if (resizeMode === "corner") {
+      columnWidth = "{e.clientX}px";
+      rowWidth = `calc(100vh - ${e.clientY}px)`;
+      columnWidth = `${e.clientX}px`;
+    }
+  };
+
+  let columnWidth = "384px";
+  let rowWidth = "384px";
+  let isResizing = false;
+  let resizeMode: "col" | "row" | "corner" | null = null;
 </script>
 
-<main class="w-full h-[100dvh] bg-neutral-950 grid grid-cols-1 grid-rows-[1fr,minmax(30vh,512px)]">
-  <div class="grid grid-cols-1 2xl:grid-cols-[minmax(384px,20vw),1fr] grid-rows-1 relative">
-    <div class="border-2 border-neutral-800 absolute 2xl:relative flex flex-col transition {isMediaPoolVisible ? 'h-1/2' : ''}">
-      <input type="file" accept="video/*" class="text-white border-2 border-neutral-800 px-3 py-1" bind:files />
-      <div class="w-full border-2 border-neutral-800 flex-1">
-        <p class="text-white">media pool</p>
-      </div>
-      <button class="2xl:hidden block w-full text-white text-center" on:click={() => (isMediaPoolVisible = !isMediaPoolVisible)}>
-        {isMediaPoolVisible ? "hide" : "show"} media pool
-      </button>
-    </div>
-    <div class="flex flex-col gap-6 justify-center items-center border-2 border-neutral-800">
-      <p class="text-white">video</p>
-      <video class="aspect-video max-w-7xl max-h-1/2 border-2 border-neutral-600" bind:this={video}>
-        {#if files}
-          <source src={URL.createObjectURL(files[0])} type={files[0].type} />
-        {/if}
-        <track kind="captions" />
-      </video>
-      <div class="w-100 flex justify-center gap-4">
-        <button class="text-white border-2 border-neutral-800 px-3 py-1" on:click={() => setVideoTime(0)}>beginning</button>
-        <button class="text-white border-2 border-neutral-800 px-3 py-1" on:click={() => (isPlaying = !isPlaying)}>{isPlaying ? "pause" : "play"}</button>
-        <button class="text-white border-2 border-neutral-800 px-3 py-1" on:click={() => setVideoTime(-1)}>end</button>
-      </div>
+<main
+  style="--col-width:minmax(320px, {columnWidth}); --row-width:minmax(calc(256px), {rowWidth}););"
+  class="w-full h-[100dvh] bg-neutral-950 grid grid-cols-[var(--col-width),3px,minmax(0,1fr)] grid-rows-[minmax(0,1fr),3px,var(--row-width)] transition-none"
+  class:select-none={isResizing}
+  on:mousemove={(e) => handleResize(e)}
+  on:mousedown={(e) => e.button === 0 && (isResizing = true)}
+  on:mouseup={() => {
+    isResizing = false;
+    resizeMode = null;
+  }}
+>
+  <div class="row-start-1 col-start-1">
+    <input type="file" accept="video/*" class="text-white" bind:files />
+    <p class="text-white">media pool</p>
+  </div>
+
+  <div class="row-start-1 col-start-3 flex flex-col justify-center items-center gap-8 p-8">
+    <p class="text-white">video</p>
+    <video class="aspect-video max-h-[75%] border-2 border-neutral-800 bg-gradient-to-br from-neutral-900 to-neutral-950" bind:this={video}>
+      {#if files}
+        <source src={URL.createObjectURL(files[0])} type={files[0].type} />
+      {/if}
+      <track kind="captions" />
+    </video>
+    <div class="w-100 flex justify-center gap-4">
+      <button class="text-white border-2 border-neutral-800 px-3 py-1" on:click={() => setVideoTime(0)}>beginning</button>
+      <button class="text-white border-2 border-neutral-800 px-3 py-1" on:click={() => (isPlaying = !isPlaying)}>{isPlaying ? "pause" : "play"}</button>
+      <button class="text-white border-2 border-neutral-800 px-3 py-1" on:click={() => setVideoTime(-1)}>end</button>
     </div>
   </div>
-  <div class="w-full h-full grid grid-cols-[minmax(256px,20vw),1fr] grid-rows-1">
-    <div class="border-2 border-neutral-800">
-      <p class="text-white">timeline</p>
-    </div>
-    <div class="border-2 border-neutral-800">
-      <p class="text-white">scrubber</p>
-      <!-- TODO: implement scrubber -->
-    </div>
+
+  <div class="row-start-3 col-start-1">
+    <p class="text-white">tracks</p>
+  </div>
+
+  <div class="row-start-3 col-start-3">
+    <p class="text-white">scrubber</p>
+  </div>
+
+  <div class="row-start-2 col-start-3 flex justify-center items-center z-10 pointer-events-none">
+    <div class="w-24 h-0.5 bg-neutral-700 rounded-full" />
+  </div>
+  <div class="relative row-start-2 col-span-full bg-neutral-800 flex justify-center items-center overflow-visible">
+    <div class="absolute w-full h-5 cursor-row-resize" on:mousedown={(e) => (resizeMode = "row")} />
+  </div>
+
+  <div class="col-start-2 row-start-1 flex justify-center items-center z-10 pointer-events-none">
+    <div class="w-0.5 h-24 bg-neutral-700 rounded-full" />
+  </div>
+  <div class="relative col-start-2 row-span-full bg-neutral-800 flex justify-center items-center overflow-visible">
+    <div class="absolute w-5 h-full cursor-col-resize" on:mousedown={(e) => (resizeMode = "col")} />
+  </div>
+
+  <div class="relative row-start-2 col-start-2 bg-neutral-600 flex justify-center items-center overflow-visible">
+    <div class="w-3 h-3 absolute bg-neutral-600" />
+    <div class="absolute w-4 h-4 cursor-move" on:mousedown={(e) => (resizeMode = "corner")} />
   </div>
 </main>
+
+<style>
+  main {
+    grid-template-rows: 1fr, 2px, [];
+  }
+</style>
