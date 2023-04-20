a1f4f32041cb971a7f453d02182ba7b35c8b21f4
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-20 01:18:22 +0100

    fix up video playback controls

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,20 @@
   import "../app.css";
 
   let isPlaying = false;
+  let video: HTMLVideoElement | null = null;
 
+  $: if (video && isPlaying) video.play();
+  $: if (video && !isPlaying) video.pause();
+
+  const setVideoTime = (time: number) => {
+    if (!video) throw new Error("Video element not found");
+
+    video.pause();
+    video.currentTime = time === -1 ? video.duration : time;
+  };
+
+  // TODO: support multiple files
   let files: FileList | null = null;
-  $: file = files && files[0];
 </script>
 
 <main class="w-full h-[100dvh] bg-neutral-950 grid grid-cols-1 grid-rows-[70vh,30vh]">
@@ -12,18 +23,19 @@
     <div class="d-flex gap-4">
       <input type="file" accept="video/*" class="text-white border-2 border-neutral-800 px-3 py-1" bind:files />
     </div>
-    <video class="aspect-video mx-auto border-2 border-neutral-800 max-h-full" controls={file !== null}>
-      {#if file}
-        <source src={URL.createObjectURL(file)} type={file.type} />
+    <video class="aspect-video mx-auto border-2 border-neutral-800 max-h-full" bind:this={video}>
+      {#if files}
+        <source src={URL.createObjectURL(files[0])} type={files[0].type} />
       {/if}
       <track kind="captions" />
     </video>
   </div>
-  <div class="m-8 border-2 border-neutral-800">
-    <div class="d-flex gap-4">
-      <button class="text-white border-2 border-neutral-800 px-3 py-1">beginning</button>
+  <div class="border-2 border-neutral-800">
+    <div class="w-100 flex justify-center gap-4">
+      <button class="text-white border-2 border-neutral-800 px-3 py-1" on:click={() => setVideoTime(0)}>beginning</button>
       <button class="text-white border-2 border-neutral-800 px-3 py-1" on:click={() => (isPlaying = !isPlaying)}>{isPlaying ? "pause" : "play"}</button>
-      <button class="text-white border-2 border-neutral-800 px-3 py-1">end</button>
+      <button class="text-white border-2 border-neutral-800 px-3 py-1" on:click={() => setVideoTime(-1)}>end</button>
     </div>
+    <div />
   </div>
 </main>
