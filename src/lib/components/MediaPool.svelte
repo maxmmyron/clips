41cad9fb5d1d777dff4f2753d22d333df08bce29
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-22 12:19:46 +0100

    implement select functionality

--- src/lib/components/MediaPool.svelte
+++ src/lib/components/MediaPool.svelte
@@ -2,9 +2,12 @@
   import MediaPoolElement from "./MediaPoolElement.svelte";
   import { mediaStore } from "../stores";
 
-  $: isAnyMediaElementSelected = $mediaStore.media.some((media) => media.isSelected);
-
+  let isAnyMediaElementSelected = false;
   let files: FileList;
+
+  mediaStore.subscribe((store) => {
+    isAnyMediaElementSelected = store.media.some((media) => media.isSelected);
+  });
 
   $: mediaStore.update((store) => {
     if (!files) return store;
@@ -18,17 +21,24 @@
 </script>
 
 <div class="relative p-2 h-full overflow-scroll">
-  <input type="file" accept=".mp4,.webm,.mpeg,.mov,.avi" class="text-white" multiple bind:files />
-  {#if isAnyMediaElementSelected}
-    <button on:click={removeSelectedElements}>trash selected</button>
-  {/if}
+  <div class="flex justify-between">
+    <input type="file" accept=".mp4,.webm,.mpeg,.mov,.avi" class="text-white h-8" multiple bind:files />
+    <button
+      class="text-white w-8 h-8 bg-neutral-700"
+      class:opacity-50={!isAnyMediaElementSelected}
+      on:click={removeSelectedElements}
+      disabled={!isAnyMediaElementSelected}>🗑️</button
+    >
+  </div>
+  <p class="absolute z-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-neutral-500">media pool</p>
   {#if files}
-    <p class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-neutral-500">media pool</p>
-    {#each $mediaStore.media as file, idx}
-      <MediaPoolElement src={file.src} {idx} on:click={() => (file.isSelected = !file.isSelected)} />
-    {/each}
-    {#each $mediaStore.media as file}
-      <p>{file.isSelected}</p>
-    {/each}
+    <div class="relative z-10">
+      {#each $mediaStore.media as file, idx}
+        <MediaPoolElement src={file.src} {idx} />
+      {/each}
+      {#each $mediaStore.media as file}
+        <p>{file.isSelected}</p>
+      {/each}
+    </div>
   {/if}
 </div>
--- src/lib/components/MediaPoolElement.svelte
+++ src/lib/components/MediaPoolElement.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
   import { mediaStore } from "../stores";
+
   export let src: string;
   export let idx: number;
 
-  const handlePreview = () => {
-    mediaStore.update((store) => {
-      store.previewIndex = idx;
-      return store;
-    });
+  let isAnyMediaElementSelected = false;
+
+  mediaStore.subscribe((store) => {
+    isAnyMediaElementSelected = store.media.some((media) => media.isSelected);
+  });
+
+  const handleClick = (e: MouseEvent) => {
+    if (e.detail === 2) {
+      mediaStore.update((store) => {
+        store.previewIndex = idx;
+        return store;
+      });
+    } else {
+      mediaStore.update((store) => {
+        store.media[idx].isSelected = !store.media[idx].isSelected;
+        return store;
+      });
+    }
   };
 </script>
 
-<button on:dblclick={handlePreview}>
+<button on:click={handleClick} class="outline-2 outline-red-200" class:outline={$mediaStore.media[idx].isSelected}>
+  {#if isAnyMediaElementSelected}
+    <input type="checkbox" checked={$mediaStore.media[idx].isSelected} />
+  {/if}
   <video>
     <source {src} type="video/mp4" />
     <track kind="captions" />
