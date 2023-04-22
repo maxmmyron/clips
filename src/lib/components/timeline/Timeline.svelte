04bb11152a14d0ea3395458c93f6a35f966abc6f
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-22 17:42:43 +0100

    implement base timeline view

--- src/lib/components/timeline/Timeline.svelte
+++ src/lib/components/timeline/Timeline.svelte
@@ -1,8 +1,13 @@
 <script>
   import { timeline } from "$lib/stores";
+  import TimelineElement from "./TimelineElement.svelte";
 </script>
 
-<div class="w-full h-full overflow-x-auto">
-  <div class="w-2/5 h-full bg-neutral-900 border-r-2 border-r-neutral-700" />
-  <div />
+<div class="w-full h-full overflow-x-auto flex">
+  <div class="w-2/5 h-full bg-neutral-900 border-r-2 border-r-neutral-700 flex-shrink-0" />
+  <div class="flex items-center">
+    {#each $timeline as options}
+      <TimelineElement {options} />
+    {/each}
+  </div>
 </div>
