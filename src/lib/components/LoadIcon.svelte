d5e06f5685f60623686155a7181ae919df6bef2f
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-27 18:46:29 +0100

    add loader

---
+++ src/lib/components/LoadIcon.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<svg width="64" height="64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="M32 8
+           A24 24 0 0 1 32 56
+           A24 24 0 0 1 8.797 34.797"
+    fill="none"
+    stroke="#000"
+    stroke-width="8"
+    stroke-linecap="round"
+    transform="rotate(-90,32,32)"
+  />
+</svg>
