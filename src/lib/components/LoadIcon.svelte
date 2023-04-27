f5b5f937bc498c864190964b78f22ee106ce3087
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-27 18:47:41 +0100

    update styling

--- src/lib/components/LoadIcon.svelte
+++ src/lib/components/LoadIcon.svelte
@@ -1,15 +1,10 @@
-<script lang="ts">
-</script>
-
-<svg width="64" height="64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+<svg width="64" height="64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="animate-spin">
   <path
     d="M32 8
            A24 24 0 0 1 32 56
            A24 24 0 0 1 8.797 34.797"
-    fill="none"
-    stroke="#000"
-    stroke-width="8"
+    class="stroke-slate-900 fill-none"
+    stroke-width="4"
     stroke-linecap="round"
-    transform="rotate(-90,32,32)"
   />
 </svg>
