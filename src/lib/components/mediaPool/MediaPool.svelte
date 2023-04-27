1956e5e1008ce5fc553a3671bbfdc985571ff6f9
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-27 01:40:59 +0100

    keep previous imports

--- src/lib/components/mediaPool/MediaPool.svelte
+++ src/lib/components/mediaPool/MediaPool.svelte
@@ -2,9 +2,16 @@
   import MediaPoolElement from "./MediaPoolElement.svelte";
   import { media, timeline } from "$lib/stores";
 
-  let files: FileList | null = null;
-
-  $: files !== null && ($media.files = Array.from(files).map((file) => ({ src: URL.createObjectURL(file), isSelected: false })));
+  const handleUpload = (e: Event & { currentTarget: EventTarget & HTMLInputElement }) => {
+    if (!(e.target as HTMLInputElement).files) return;
+    const files: FileList = (e.target as HTMLInputElement).files as FileList;
+    $media.files = [
+      ...$media.files,
+      ...Array.from(files)
+        .filter((file) => $media.files.every((f) => f.name !== file.name))
+        .map((file) => ({ src: URL.createObjectURL(file), isSelected: false, name: file.name })),
+    ];
+  };
 
   const handleKey = (e: KeyboardEvent) => {
     if (e.key !== "Delete") return;
@@ -24,16 +31,14 @@
 
 <div class="relative pt-4 px-4 h-full overflow-scroll">
   <div class="flex justify-between gap-2">
-    <input type="file" accept=".mp4,.webm,.mpeg,.mov,.avi" class="text-white h-8" multiple bind:files />
+    <input type="file" accept=".mp4,.webm,.mpeg,.mov,.avi" class="text-white h-8" multiple on:change={handleUpload} />
   </div>
   <p class="absolute z-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-neutral-500">media pool</p>
-  {#if files}
-    <div class="w-full flex flex-wrap gap-3">
-      {#key $media.files.length}
-        {#each $media.files as file, idx}
-          <MediaPoolElement src={file.src} {idx} />
-        {/each}
-      {/key}
-    </div>
-  {/if}
+  <div class="w-full flex flex-wrap gap-3">
+    {#key $media.files.length}
+      {#each $media.files as file, idx}
+        <MediaPoolElement src={file.src} {idx} />
+      {/each}
+    {/key}
+  </div>
 </div>
