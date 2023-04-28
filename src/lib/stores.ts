b6bd4b57363303c6edb421091562baf964c1e603
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-04-28 13:23:16 +0100

    update store and types

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,32 +2,56 @@ import {spring, type Spring} from "svelte/motion";
 import { writable } from "svelte/store";
 
 export const studio = writable({
+  /**
+   * The current resize direction. Used to determine cursor stile and resize logic. If null, no resizing will occur.
+   */
   resizeMode: null,
+  /**
+   * The drag data. used to determine what media is being dragged, and how to transform it on the screen given certain drag actions.
+   * If null, no dragging is occurring.
+   */
   dragData: null,
+  /**
+   * The current mouse position, represented as a spring.
+   */
   mouse: spring({x:0, y:0}),
 } as {
   resizeMode: "row" | "mediaCol" | "timelineCol" | null;
-  dragData: MediaDragData | null;
+  dragData: StudioMediaMetadata | null;
   mouse: Spring<{x:number,y:number}>;
 });
 
-export const media = writable({
-  previewSource: "",
+export const mediaPool = writable({
+  /**
+   * The current preview media. Used to determine what media to display on the preview video element.
+   * If null, no preview is being shown.
+   */
+  previewSrc: null,
+  /**
+   * An array of currently selected media indexes. Used to determine what media to display as selected in the media pool.
+   */
   selected: [],
-  files: [],
+  /**
+   * An array of loaded media. Carries a variety of information that can be used to display media details and preview media.
+   */
+  media: [],
 } as {
-  previewSource: string;
+  previewSrc: StudioMediaMetadata | null;
   selected: number[];
-  files: MediaPoolFile[];
+  media: StudioMediaMetadata[];
 });
 
 export const timeline = writable({
+  /**
+   * An array of currently selected clip indexes. Used to determine what clips to display as selected in the timeline.
+   */
   selected: [],
+  /**
+   * An array of loaded clips. Carries a variety of information that can be used to display clip details and preview clips.
+   */
   clips: [],
-  thumbnails: new Map(),
 } as {
   selected: number[];
-  clips: Omit<Media, "isSelected" > [];
-  thumbnails: Map<string, string>;
+  clips: StudioTimelineMetadata[];
 });
 
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,20 +1,37 @@
 enum DropLocation { PLAYER, TIMELINE };
 
-interface Media {
+interface StudioMediaMetadata {
+  /**
+   * The media source URL.
+   */
   src: string;
-  duration: number;
-  startOffset: number;
-  endOffset: number;
-  isSelected: boolean;
-}
-
-type MediaDragData = {
-  src: string,
-  duration: number,
-}
-
-type MediaPoolFile = {
-  src: string;
+  /**
+   * The name of the file. Used as an identifier for the media, and as the display name in the media pool.
+   */
   name: string;
-  isSelected: boolean;
+  /**
+   * The media duration in seconds. Used to determine the length of the clip in the timeline.
+   */
+  duration: Promise<number>;
+  /**
+   *  A series of base64 encoded images to use as thumbnails.
+   */
+  thumbnails: Promise<string[]>;
+  /**
+   * The audio buffer source node. Used to play the audio and generate a timeline waveform preview.
+   */
+  audioBufferSourceNode: Promise<AudioBufferSourceNode>;
 }
+
+interface startTimeMetadata {
+  /**
+   * The start time of the clip in seconds.
+   */
+  startTime: number;
+  /**
+   * The end time of the clip in seconds.
+   */
+  endTime: number;
+}
+
+type StudioTimelineMetadata = StudioMediaMetadata & startTimeMetadata;
