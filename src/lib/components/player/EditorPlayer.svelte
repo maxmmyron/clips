f562970aebd416d2c1eb2ef09b9621a63d863d55
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-05-04 23:57:07 +0100

    decreasing streaming lag using two video sources

--- src/lib/components/player/EditorPlayer.svelte
+++ src/lib/components/player/EditorPlayer.svelte
@@ -1,72 +1,81 @@
 <script lang="ts">
   import { timeline } from "$lib/stores";
   import { Canvas, Layer, t, type Render } from "svelte-canvas";
+  import { onMount } from "svelte";
 
-  export let width = 640;
-  export let height = 480;
+  export let width = 640,
+    height = 480;
 
-  let currentSrcIndex = 0;
-  let accumulatedTime = 0;
-  let currentTime = 0;
   let isPaused = true;
-  let video: HTMLVideoElement;
-  let render: Render;
+  let srcAIndex = 0,
+    srcBIndex = 1;
 
-  $: if (video) isPaused, isPaused ? video.pause() : video.play();
+  let trackA: HTMLVideoElement, trackB: HTMLVideoElement;
+  let preview: HTMLVideoElement;
+
+  onMount(() => (preview = trackA));
+
+  $: if (preview) isPaused, isPaused ? preview.pause() : preview.play();
+  $: srcA = $timeline.clips[srcAIndex]?.src;
+  $: srcB = $timeline.clips[srcBIndex]?.src;
+  $: currentTime = preview?.currentTime || 0;
+  $: accumulatedTime = $timeline.clips.slice(0, Math.max(srcAIndex, srcBIndex)).reduce((acc, cur) => acc + cur.duration, 0);
 
   const handleEnded = () => {
-    accumulatedTime += video.duration;
-    if (currentSrcIndex === $timeline.clips.length - 1) {
+    if ((preview === trackA ? srcAIndex : srcBIndex) === $timeline.clips.length - 1) {
       isPaused = true;
       return;
     }
-    currentSrcIndex = currentSrcIndex + 1;
+
+    if (preview === trackA) {
+      preview = trackB;
+      srcAIndex = srcBIndex + 1;
+    } else {
+      preview = trackA;
+      srcBIndex = srcAIndex + 1;
+    }
   };
 
+  let render: Render;
   $: render = ({ context, width, height }) => {
     $t;
-    if (!video) {
+    if (!preview) {
       console.warn("No video to render");
       return;
     }
 
     const mediaSize = {
-      width: video.videoWidth * Math.min(width / video.videoWidth, height / video.videoHeight),
-      height: video.videoHeight * Math.min(width / video.videoWidth, height / video.videoHeight),
+      width: preview.videoWidth * Math.min(width / preview.videoWidth, height / preview.videoHeight),
+      height: preview.videoHeight * Math.min(width / preview.videoWidth, height / preview.videoHeight),
     };
 
-    const mediaPosition = {
-      x: Math.max(0, (width - mediaSize.width) / 2),
-      y: Math.max(0, (height - mediaSize.height) / 2),
-    };
+    const mediaPosition: [number, number] = [Math.max(0, (width - mediaSize.width) / 2), Math.max(0, (height - mediaSize.height) / 2)];
 
-    context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, mediaPosition.x, mediaPosition.y, mediaSize.width, mediaSize.height);
+    context.drawImage(preview, 0, 0, preview.videoWidth, preview.videoHeight, ...mediaPosition, mediaSize.width, mediaSize.height);
   };
 
   function setPlayerTime(front: boolean = true): any {
     isPaused = true;
     if (front) {
-      currentSrcIndex = 0;
-      accumulatedTime = 0;
+      (srcAIndex = 0), (srcBIndex = 1);
+      currentTime = 0;
     } else {
-      currentSrcIndex = $timeline.clips.length - 1;
-      accumulatedTime = $timeline.clips.reduce((acc, cur) => acc + cur.duration, 0) - video.duration;
-      currentTime = video.duration;
+      // TODO: kinda shitty code; probably should directly set src attribute of preview. Works for now but consider refactoring
+      preview.src = $timeline.clips[$timeline.clips.length - 1].src;
+      currentTime = preview.duration;
+      if (preview === trackA) {
+        srcAIndex = $timeline.clips.length - 1;
+        srcBIndex = $timeline.clips.length;
+      } else {
+        srcAIndex = $timeline.clips.length;
+        srcBIndex = $timeline.clips.length - 1;
+      }
     }
   }
 </script>
 
-<video
-  class="hidden"
-  muted
-  bind:this={video}
-  src={$timeline.clips[currentSrcIndex]?.src}
-  on:ended={handleEnded}
-  on:canplay={() => !isPaused && video.play()}
-  bind:currentTime
->
-  <track kind="captions" />
-</video>
+<video class="hidden" muted src={srcA} bind:this={trackA} on:ended={handleEnded} />
+<video class="hidden" muted src={srcB} bind:this={trackB} on:ended={handleEnded} />
 
 <div class="w-full h-full flex justify-center items-center">
   {#if $timeline.clips.length}
