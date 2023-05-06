f792d04456fa444d2de67dfb28d9697e4d434363
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-05-06 11:49:25 +0100

    fix overlapping media elements

--- src/lib/components/mediaPreview/MediaVideoPreview.svelte
+++ src/lib/components/mediaPreview/MediaVideoPreview.svelte
@@ -8,7 +8,7 @@
 
 <div class="w-full {isTimelineElement ? 'h-1/2' : 'h-full'} flex rounded-md overflow-clip">
   {#each metadata.thumbnails as thumbnail}
-    <div class="h-full aspect-video" style="background-image:url({thumbnail}); background-size: auto 100%;" />
+    <div class="h-full aspect-video bg-no-repeat bg-center bg-black" style="background-image:url({thumbnail}); background-size: auto 100%;" />
   {/each}
   <!-- TODO: display media name -->
 </div>
