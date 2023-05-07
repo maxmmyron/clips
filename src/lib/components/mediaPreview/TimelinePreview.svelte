7e9a3197f8ed56efd970c12146b50dce973db402
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-05-07 23:49:45 +0100

    dirty implementation of offset adjustment

--- src/lib/components/mediaPreview/TimelinePreview.svelte
+++ src/lib/components/mediaPreview/TimelinePreview.svelte
@@ -4,18 +4,23 @@
   export let metadata: TimelineMedia;
 
   let duration = metadata.duration;
-  $: width = (duration - metadata.startTime - metadata.endTime) * $timeline.zoomScale ** 1.75 + "px";
+  let isAdjustingOffsets = false;
   let mediaPreview: HTMLButtonElement;
 
   $: isSelected = $timeline.selected.includes(metadata);
+  $: width = (duration - metadata.startTime - metadata.endTime) * $timeline.zoomScale ** 1.75 + "px";
 
   const handleClick = (e: MouseEvent) => {
+    if (isAdjustingOffsets) {
+      isAdjustingOffsets = false;
+      return;
+    }
     if (e.detail == 2) $player.source = metadata.src;
     else if (e.shiftKey) $timeline.selected = [...$timeline.selected, metadata];
     else $timeline.selected = [metadata];
   };
 
-  const handleDragStart = (e: MouseEvent) => {
+  const handleReorder = (e: MouseEvent) => {
     $studio.dragData = {
       ...$studio.dragData,
       media: metadata,
@@ -30,15 +35,40 @@
 
     $timeline.dragIndex = $timeline.clips.indexOf(metadata);
   };
+
+  const handleOffsetAdjust = (e: MouseEvent, offsetIndex: number) => {
+    // TODO: implement
+    // $studio.dragData = {
+    //   ...$studio.dragData,
+    //   media: metadata,
+    //   originType: "timeline",
+    //   originPosition: { x: e.clientX, y: e.clientY },
+    //   dragEvent: "dragstart",
+    //   currentDragRegion: "timeline",
+    // };
+
+    const offset = offsetIndex === 0 ? "startTime" : "endTime";
+    const currentOffset = metadata[offset];
+
+    console.log(e.clientX, mediaPreview.getBoundingClientRect().x);
+  };
 </script>
 
 <button
   bind:this={mediaPreview}
   style="width: {width};"
   on:click|capture|stopPropagation={handleClick}
-  class="relative flex flex-col outline-2 outline-blue-600 w-48 rounded-md overflow-clip h-48 bg-black"
+  class="relative flex flex-col outline-2 outline-blue-600 w-48 rounded-md overflow-clip h-48 bg-black group"
   class:outline={isSelected}
-  on:mousedown={handleDragStart}
+  on:mousedown={handleReorder}
 >
+  <button
+    class="z-10 absolute h-full left-0 w-2 bg-emerald-950 opacity-0 cursor-col-resize group-hover:opacity-100"
+    on:mousedown|stopPropagation={(e) => handleOffsetAdjust(e, 0)}
+  />
   <slot />
+  <button
+    class="z-10 absolute h-full right-0 w-2 bg-emerald-950 opacity-0 cursor-col-resize group-hover:opacity-100"
+    on:mousedown|stopPropagation={(e) => handleOffsetAdjust(e, 1)}
+  />
 </button>
