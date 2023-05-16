8efcc5fd7d9f36857fd1bc0ad36a9cca5b920990
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-05-16 19:21:30 +0100

    update store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -26,7 +26,7 @@ export const studio = writable({
   resizeMode: "row" | "mediaCol" | "timelineCol" | null;
   audioContext: AudioContext | null;
   dragData: {
-    media: UploadedMedia<VideoMetadata | AudioMetadata | ImageMetadata> | null;
+    media: UploadedVideo | UploadedAudio | UploadedImage | null;
     originType: "mediaPool" | "timeline" | null;
     originPosition: {x:number, y:number} | null;
     dragEvent: "dragstart" | "drag" | "dragend" | null;
