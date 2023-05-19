d7615fbaa30f677b8fa2679b8f75cf5e1460d533
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-05-19 18:28:05 +0100

    finally fix the stupid buffer system

--- src/lib/components/player/Buffer.svelte
+++ src/lib/components/player/Buffer.svelte
@@ -10,26 +10,25 @@
   let video: HTMLVideoElement, image: HTMLImageElement, audioNode: AudioBufferSourceNode | undefined;
 
   /**
-   * A private timestamp of the timestamp at last pause. Used to calculate value for accumulatedPauseOffset.
+   * The audioContext timestamp when the player was paused.
    */
-  let lastPauseTimestamp = 0,
-    lastStartOffset = 0;
+  let pauseTimestamp = 0;
+  let lastPauseState: "paused" | "playing" = "paused";
 
   // handle video playback if dealing with video el
   $: if (node === $timeline.curr && video) !$player.isPaused ? video.play() : video.pause();
   $: if (audioNode && $player.isPaused) audioNode.disconnect();
+  $: if (node === $timeline.curr && $player.isPaused && lastPauseState === "playing") {
+    lastPauseState = "paused";
+    pauseTimestamp = metadata.hasStarted ? audioContext.currentTime : 0;
+  }
 
-  // update timestamps
-  $: $player.isPaused && (lastPauseTimestamp = audioContext.currentTime);
-
-  if (audioNode && node.uuid == $timeline.curr?.uuid)
-    audioNode.addEventListener("ended", () => {
-      console.log("ended");
-      audioNode && audioNode.disconnect();
-      metadata.hasEnded = true;
-      if (node === $timeline.clips.tail) $player.isPaused = true;
-      else $timeline.curr = node.next;
-    });
+  $: if (metadata.runtime >= metadata.duration - metadata.offsets[1] - metadata.offsets[0] && node.uuid == $timeline.curr?.uuid) {
+    audioNode && audioNode.disconnect();
+    metadata.hasEnded = true;
+    if (node === $timeline.clips.tail) $player.isPaused = true;
+    else $timeline.curr = node.next;
+  }
 
   const handlePlay = () => {
     audioNode = audioContext.createBufferSource();
@@ -38,33 +37,28 @@
       const audioNodeBuffer = audioContext.createBuffer(metadata.audio.numberOfChannels, metadata.audio.length, metadata.audio.sampleRate);
       for (let i = 0; i < metadata.audio.numberOfChannels; i++) audioNodeBuffer.copyToChannel(metadata.audio.getChannelData(i), i);
       audioNode.buffer = audioNodeBuffer;
-    } else {
-      // create empty audio buffer for image
-      audioNode.buffer = audioContext.createBuffer(1, metadata.duration * 44100, 44100);
-    }
+    } else audioNode.buffer = audioContext.createBuffer(1, metadata.duration * 44100, 44100);
 
-    let currCtxTimestamp = audioContext.currentTime;
+    lastPauseState = "playing";
+    metadata.hasStarted = true;
 
-    let startOffset = metadata.offsets[0];
-    if (!metadata.hasStarted) {
-      metadata.startTimestamp = currCtxTimestamp;
-      metadata.hasStarted = true;
-    } else {
-      startOffset = metadata.offsets[0] + metadata.runtime;
-      metadata.accumulatedPauseOffset += currCtxTimestamp - lastPauseTimestamp;
-    }
+    let startOffset = metadata.offsets[0] + metadata.runtime;
+    if (!metadata.hasStarted) metadata.initialTimestamp = audioContext.currentTime;
+    else metadata.pauseAccumulator += audioContext.currentTime - pauseTimestamp;
 
-    lastStartOffset = currCtxTimestamp;
-
-    const duration = metadata.duration - metadata.offsets[1] - startOffset;
-    console.log(`playing from ${startOffset}s to ${metadata.duration - metadata.offsets[1]}s (${duration} seconds)`);
     audioNode.connect(audioContext.destination);
-    audioNode.start(0, startOffset, duration);
+    audioNode.start(0, startOffset, metadata.duration - metadata.offsets[1] - startOffset);
   };
 
   onMount(() => {
     if (!video && !image) throw new Error("No buffer to mount.");
     $timeline.buffers.set(node.uuid, video || image);
+
+    node.metadata.setMediaTime = (time: number) => {
+      if (video) video.currentTime = time;
+      audioNode?.disconnect();
+      pauseTimestamp = 0;
+    };
   });
 </script>
 
