dc2c689bf83779c09d4eec71ad44f9c20d8883d3
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-05-25 15:19:37 +0100

    simplify editor logic

--- src/lib/components/player/EditorPlayer.svelte
+++ src/lib/components/player/EditorPlayer.svelte
@@ -3,7 +3,6 @@
   import { Canvas, Layer, t, type Render } from "svelte-canvas";
   import { onMount } from "svelte";
   import Buffer from "./Buffer.svelte";
-  import { MediaType } from "$lib/exports";
 
   export let width = 640,
     height = 480;
@@ -14,33 +13,23 @@
 
   onMount(() => (audioContext = new AudioContext()));
 
-  $: console.log($timeline.curr);
+  $: console.log($timeline.current);
 
-  $: if ($timeline.curr) {
-    const accumulatorClips = $timeline.clips.toArray().slice(0, $timeline.clips.indexOf($timeline.curr.uuid));
-    accumulatedTime = accumulatorClips.reduce((acc, { metadata }) => acc + (metadata.duration - metadata.offsets[0] - metadata.offsets[1]), 0);
+  $: if ($timeline.current) {
+    const accumulatorClips = $timeline.timeline.toArray().slice(0, $timeline.timeline.indexOf($timeline.current.uuid));
+    accumulatedTime = accumulatorClips.reduce((acc, { metadata }) => acc + (metadata.duration - metadata.start - metadata.end), 0);
   }
-
-  // update current clip on add
-  $: if (!$timeline.curr && $timeline.clips.length > 0) $timeline.curr = $timeline.clips.head;
 
   let render: Render;
   $: render = ({ context, width, height }) => {
     $t;
-    if ($timeline.clips.length === 0 || !$timeline.curr) return;
-    const bufferEl = $timeline.buffers.get($timeline.curr.uuid);
+    if ($timeline.timeline.length === 0 || !$timeline.current) return;
+    const bufferEl = $timeline.sources.get($timeline.current.uuid)?.source;
     if (!bufferEl || !audioContext) return;
 
-    const metadata = $timeline.curr.metadata;
-    if (!$player.isPaused) {
-      console.log("setting");
-      metadata.runtime = audioContext.currentTime - (metadata.initialTimestamp + metadata.pauseAccumulator);
-    }
-    previewTime = metadata.runtime;
-
     // TODO: try to remove video width calculations outside of render so we aren't recomputing this every frame
-    const bufferWidth = metadata.type === MediaType.VIDEO ? (bufferEl as HTMLVideoElement).videoWidth : bufferEl.width;
-    const bufferHeight = metadata.type === MediaType.VIDEO ? (bufferEl as HTMLVideoElement).videoHeight : bufferEl.height;
+    const bufferWidth = $timeline.current.type === "video" ? (bufferEl as HTMLVideoElement).videoWidth : bufferEl.width;
+    const bufferHeight = $timeline.current.type === "video" ? (bufferEl as HTMLVideoElement).videoHeight : bufferEl.height;
 
     const mediaSize = {
       width: bufferWidth * Math.min(width / bufferWidth, height / bufferHeight),
@@ -56,42 +45,21 @@
     console.log(`skipping to ${front ? "front" : "back"}`);
     $player.isPaused = true;
 
-    console.log($timeline.clips.toArray().map((clip) => clip.metadata.name));
-
     audioContext = new AudioContext();
 
-    $timeline.clips.toArray().forEach((clip) => {
-      const metadata = clip.metadata;
-      metadata.runtime = front ? 0 : metadata.duration - metadata.offsets[1];
-      metadata.setMediaTime(front ? clip.metadata.offsets[0] : clip.metadata.duration - clip.metadata.offsets[1]);
-      metadata.pauseAccumulator = 0;
-      if (front) metadata.initialTimestamp = audioContext.currentTime;
-      else metadata.initialTimestamp = audioContext.currentTime - metadata.duration;
-
-      clip.metadata.hasEnded = !front;
-      clip.metadata.hasStarted = !front;
-
-      console.log(`clip ${metadata.name} runtime: ${metadata.runtime}`);
-    });
-
-    $timeline.curr = front ? $timeline.clips.head : $timeline.clips.tail;
+    $timeline.runtime = front ? 0 : $timeline.duration;
+    $timeline.current = front ? $timeline.timeline.head : $timeline.timeline.tail;
   }
-
-  const togglePlayState = () => {
-    if (!$timeline.curr) return;
-    if (!$timeline.clips.tail) throw new Error("Timeline tail is null... Something has gone horribly wrong. (Button was clicked despite timeline being empty)");
-    if ($timeline.curr.uuid === $timeline.clips.tail.uuid && $timeline.curr.metadata.hasEnded) setPlayerTime();
-    $player.isPaused = !$player.isPaused;
-  };
 </script>
 
 <!-- TODO: this *will* filter out audio nodes, but a type error is thrown unless the Buffer.svelte element accepts all node types. Need to investigate and fix (shoddy types) -->
-{#each $timeline.clips.toArray().filter((node) => node.metadata.type !== MediaType.AUDIO) as node}
-  <Buffer {audioContext} {node} />
+<!-- TODO: add node type functions to helpers.ts -->
+{#each $timeline.timeline.toArray().filter((node) => node.type !== "audio") as node}
+  <Buffer {audioContext} nodeUUID={node.uuid} />
 {/each}
 
 <div class="w-full h-full flex justify-center items-center">
-  {#if $timeline.clips.length}
+  {#if $timeline.timeline.length}
     <Canvas {width} {height}>
       <Layer {render} />
     </Canvas>
@@ -100,28 +68,20 @@
   {/if}
 </div>
 
-<p class="w-full text-white text-right">{Math.round((accumulatedTime + previewTime) * 100) / 100 || 0}</p>
+<p class="w-full text-white text-right">{Math.round($timeline.runtime * 100) / 100 || 0}</p>
 
 <div class="w-full flex justify-center gap-4">
-  <button
-    class="text-white border-2 border-neutral-800 px-3 py-1 disabled:opacity-50 disabled:cursor-not-allowed"
-    on:click={() => setPlayerTime()}
-    disabled={$timeline.curr === null || ($timeline.curr === $timeline.clips.head && !$timeline.curr.metadata.hasStarted)}
-  >
+  <button class="text-white border-2 border-neutral-800 px-3 py-1 disabled:opacity-50 disabled:cursor-not-allowed" on:click={() => setPlayerTime()}>
     ⏪
   </button>
   <button
     class="text-white border-2 border-neutral-800 px-3 py-1 disabled:opacity-50 disabled:cursor-not-allowed"
-    on:click={togglePlayState}
-    disabled={$timeline.curr === null}
+    on:click={() => ($player.isPaused = !$player.isPaused)}
+    disabled={$timeline.current === null}
   >
     {$player.isPaused ? "▶️" : "⏸️"}
   </button>
-  <button
-    class="text-white border-2 border-neutral-800 px-3 py-1 disabled:opacity-50 disabled:cursor-not-allowed"
-    on:click={() => setPlayerTime(false)}
-    disabled={$timeline.curr === null || ($timeline.curr === $timeline.clips.tail && $timeline.curr.metadata.hasEnded)}
-  >
+  <button class="text-white border-2 border-neutral-800 px-3 py-1 disabled:opacity-50 disabled:cursor-not-allowed" on:click={() => setPlayerTime(false)}>
     ⏩
   </button>
 </div>
