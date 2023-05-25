749d2218e5908db7538b8c8106285a8102694134
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-05-25 03:40:23 +0100

    fix up import, drag, types

--- src/lib/components/Timeline.svelte
+++ src/lib/components/Timeline.svelte
@@ -4,17 +4,17 @@
   import MediaVideoPreview from "./mediaPreview/MediaVideoPreview.svelte";
   import MediaAudioPreview from "./mediaPreview/MediaAudioPreview.svelte";
   import TimelinePreview from "./mediaPreview/TimelinePreview.svelte";
-  import { MediaType } from "$lib/exports";
+  import { isAudioMedia, isVideoMedia } from "./util/helpers";
 
-  let zoom = 5;
+  let zoomScale = 5;
   let timelineContainer: HTMLElement;
 
   let dropIndex: number = -1;
 
-  $: $timeline.zoom = zoom;
+  $: $timeline.zoomScale = zoomScale;
 
   const handleDrag = (e: MouseEvent) => {
-    if ($studio.dragData.dragEvent !== "drag" || $studio.dragData.currentDragRegion !== "timeline") return;
+    if ($studio.draggable.event !== "drag" || $studio.draggable.current.region !== "timeline") return;
 
     // get the element to place our element before
     const afterElement = [...timelineContainer.querySelectorAll(".draggable:not(.dragging)")].reduce(
@@ -43,71 +43,47 @@
     }
 
     // update ghost position and size
-    $studio.dragData.ghost.position.set({
+    $studio.draggable.ghost.pos.set({
       x: dropXPosition,
       y: timelineContainer.getBoundingClientRect().top,
     });
-    $studio.dragData.ghost.size.set({ width: 8, height: timelineContainer.getBoundingClientRect().height });
+    $studio.draggable.ghost.size.set({ width: 8, height: timelineContainer.getBoundingClientRect().height });
   };
 
   const handleDragEnd = () => {
-    if ($studio.dragData.dragEvent === "dragstart") return;
-    if ($studio.dragData.originType === "timeline") {
+    if ($studio.draggable.event === "start") return;
+    if ($studio.draggable.origin?.region === "timeline") {
       if ($timeline.dragIndex === -1) return;
 
-      let clip = $timeline.clips.getAt($timeline.dragIndex) as TimelineLayerNode;
-      $timeline.clips.remove(clip.uuid);
+      let clip = $timeline.timeline.getByIndex($timeline.dragIndex) as App.Node;
+      $timeline.timeline.remove(clip.uuid);
 
-      if (dropIndex === -1) $timeline.clips.add(clip);
-      else $timeline.clips.add(clip, dropIndex);
+      if (dropIndex === -1) $timeline.timeline.add(clip);
+      else $timeline.timeline.add(clip, dropIndex);
 
       $timeline.dragIndex = -1;
       return;
     }
     // handle dragging new media into timeline
 
-    if (!$studio.dragData.media) return;
+    if (!$studio.draggable.media) return;
+    const draggable = $studio.draggable.media;
 
-    const dragData = $studio.dragData.media;
+    let duration = 3;
+    if (isVideoMedia(draggable) || isAudioMedia(draggable)) {
+      duration = draggable.metadata.duration;
+    }
 
-    const generateMetadata = (dragData: UploadedVideo | UploadedAudio | UploadedImage): TimelineVideo | TimelineAudio | TimelineImage => {
-      const defaultMetadata = {
-        src: dragData.src,
-        name: dragData.name,
-        duration: dragData.type !== MediaType.IMAGE ? dragData.duration : 3,
-        offsets: [0, 0],
-        hasStarted: false,
-        hasEnded: false,
-        pauseAccumulator: 0,
-        runtime: 0,
-        initialTimestamp: 0,
-      } as TimelineVideo | TimelineAudio | TimelineImage;
-
-      switch (dragData.type) {
-        case MediaType.VIDEO:
-          return {
-            ...defaultMetadata,
-            audio: dragData.audio,
-            thumbnails: dragData.thumbnails,
-            type: MediaType.VIDEO,
-          } as TimelineVideo;
-        case MediaType.AUDIO:
-          return {
-            ...defaultMetadata,
-            audio: dragData.audio,
-            type: MediaType.AUDIO,
-          } as TimelineAudio;
-        case MediaType.IMAGE:
-          return {
-            ...defaultMetadata,
-            type: MediaType.IMAGE,
-          } as TimelineImage;
-      }
-    };
-
-    $timeline.clips.add({
+    $timeline.timeline.add({
       uuid: uuidv4(),
-      metadata: generateMetadata(dragData),
+      type: draggable.type,
+      src: draggable.src,
+      metadata: {
+        title: draggable.metadata.title,
+        duration: duration,
+        start: 0,
+        end: 0,
+      },
       next: null,
       prev: null,
     });
@@ -116,8 +92,7 @@
   const handleKey = (e: KeyboardEvent) => {
     if (e.key !== "Delete") return;
 
-    const deleteIDs = $timeline.selected.map((clip) => clip.uuid);
-    deleteIDs.forEach((id) => $timeline.clips.remove(id));
+    $timeline.selected.forEach((uuid) => $timeline.timeline.remove(uuid));
 
     $timeline.selected = [];
   };
@@ -130,34 +105,34 @@
   <div
     class="w-full h-full flex items-center"
     on:mouseup={handleDragEnd}
-    on:mouseenter={() => ($studio.dragData.currentDragRegion = "timeline")}
-    on:mouseleave={() => ($studio.dragData.currentDragRegion = null)}
+    on:mouseenter={() => ($studio.draggable.current.region = "timeline")}
+    on:mouseleave={() => ($studio.draggable.current.region = null)}
   >
     <div class="flex h-fit min-h-[50%]" bind:this={timelineContainer}>
-      {#each $timeline.clips.toArray() as node, idx (node.uuid)}
+      {#each $timeline.timeline.toArray() as node, idx (node.uuid)}
         <div
           class="draggable overflow-clip"
-          class:dragging={idx === $timeline.dragIndex && $studio.dragData.dragEvent === "drag"}
-          class:w-0={idx === $timeline.dragIndex && $studio.dragData.dragEvent === "drag"}
+          class:dragging={idx === $timeline.dragIndex && $studio.draggable.event === "drag"}
+          class:w-0={idx === $timeline.dragIndex && $studio.draggable.event === "drag"}
         >
           <TimelinePreview {node}>
-            {#if node.metadata.type === MediaType.VIDEO}
+            {#if node.type === "video"}
               <MediaVideoPreview metadata={node.metadata} isTimelineElement={true} />
               <MediaAudioPreview
                 metadata={{
                   ...node.metadata,
-                  type: MediaType.AUDIO,
+                  type: "audio",
                 }}
               />
-            {:else if node.metadata.type === MediaType.AUDIO}
+            {:else if node.type === "audio"}
               <div class="h-full" />
               <MediaAudioPreview metadata={node.metadata} />
-            {:else if node.metadata.type === MediaType.IMAGE}
+            {:else if node.type === "image"}
               <MediaVideoPreview
                 metadata={{
                   ...node.metadata,
-                  type: MediaType.VIDEO,
-                  thumbnails: [node.metadata.src],
+                  type: "video",
+                  thumbnails: [node.src],
                   audio: new AudioBuffer({ length: node.metadata.duration, sampleRate: 44100 }),
                 }}
                 isTimelineElement={true}
@@ -169,5 +144,5 @@
       {/each}
     </div>
   </div>
-  <input class="absolute top-2 right-2" type="range" min="1" max="10" bind:value={zoom} />
+  <input class="absolute top-2 right-2" type="range" min="1" max="10" bind:value={zoomScale} />
 </div>
