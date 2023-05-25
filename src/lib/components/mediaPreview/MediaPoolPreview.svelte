e70084990b5a8100ae472a1118bb138b6b0e5016
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-05-25 03:57:23 +0100

    fix types

--- src/lib/components/mediaPreview/MediaPoolPreview.svelte
+++ src/lib/components/mediaPreview/MediaPoolPreview.svelte
@@ -1,33 +1,40 @@
 <script lang="ts">
   import { player, studio, mediaPool } from "$lib/stores";
 
-  export let metadata: UploadedVideo | UploadedAudio | UploadedImage;
+  export let media: App.VideoMedia | App.AudioMedia | App.ImageMedia;
 
   let mediaPreview: HTMLButtonElement;
   let containerWidth: number, nameWidth: number;
 
-  $: isSelected = $mediaPool.selected.includes(metadata);
+  $: isSelected = $mediaPool.selected.includes(media.uuid);
   $: shouldNameAnimate = nameWidth > containerWidth;
   $: nameOffset = containerWidth - nameWidth - 16;
 
   const handleClick = (e: MouseEvent) => {
-    if (e.detail == 2) $player.source = metadata.src;
-    else if (e.shiftKey) $mediaPool.selected = [...$mediaPool.selected, metadata];
-    else $mediaPool.selected = [metadata];
+    if (e.detail == 2) $player.source = media.src;
+    else if (e.shiftKey) $mediaPool.selected = [...$mediaPool.selected, media.uuid];
+    else $mediaPool.selected = [media.uuid];
   };
 
   const handleDragStart = (e: MouseEvent) => {
-    $studio.dragData = {
-      ...$studio.dragData,
-      media: metadata,
-      originType: "mediaPool",
-      originPosition: { x: e.clientX, y: e.clientY },
-      dragEvent: "dragstart",
-      currentDragRegion: null,
+    $studio.draggable = {
+      ...$studio.draggable,
+      media: media,
+      origin: {
+        pos: { x: e.clientX, y: e.clientY },
+        region: "media_pool",
+      },
+      event: "start",
+      current: {
+        region: null,
+      },
     };
 
-    $studio.dragData.ghost.position.set({ x: mediaPreview.getBoundingClientRect().x, y: mediaPreview.getBoundingClientRect().y }, { hard: true });
-    $studio.dragData.ghost.size.set({ width: mediaPreview.getBoundingClientRect().width, height: mediaPreview.getBoundingClientRect().height }, { hard: true });
+    $studio.draggable.ghost.pos.set({ x: mediaPreview.getBoundingClientRect().x, y: mediaPreview.getBoundingClientRect().y }, { hard: true });
+    $studio.draggable.ghost.size.set(
+      { width: mediaPreview.getBoundingClientRect().width, height: mediaPreview.getBoundingClientRect().height },
+      { hard: true }
+    );
   };
 </script>
 
@@ -41,7 +48,7 @@
   <slot />
   <div class="absolute py-1 px-2 bg-[rgba(0,0,0,0.4)] backdrop-blur-sm w-full bottom-0 overflow-clip" bind:clientWidth={containerWidth}>
     <p style="--overflow-scroll-pos: {nameOffset}px" class="relative text-white m-0 w-max {shouldNameAnimate && 'animate'}" bind:clientWidth={nameWidth}>
-      {metadata.name}
+      {media.metadata.title}
     </p>
   </div>
 </button>
