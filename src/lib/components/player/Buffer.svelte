dc46002f7163be29c13ca223b3dc370276a364b1
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-05-25 15:06:22 +0100

    add temp buffer logic

--- src/lib/components/player/Buffer.svelte
+++ src/lib/components/player/Buffer.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-  import { MediaType } from "$lib/exports";
   import { player, timeline } from "$lib/stores";
   import { onMount } from "svelte";
 
   // TODO: fix this so we only accept video and image buffers.
-  export let node: TimelineLayerNode<TimelineVideo | TimelineImage | TimelineAudio>, audioContext: AudioContext;
+  export let nodeUUID: string, audioContext: AudioContext;
 
+  const node = $timeline.timeline.getByUUID(nodeUUID) as App.Node;
   const metadata = node.metadata;
+
   let video: HTMLVideoElement, image: HTMLImageElement, audioNode: AudioBufferSourceNode | undefined;
 
   /**
@@ -16,53 +17,58 @@
   let lastPauseState: "paused" | "playing" = "paused";
 
   // handle video playback if dealing with video el
-  $: if (node === $timeline.curr && video) !$player.isPaused ? video.play() : video.pause();
-  $: if (audioNode && $player.isPaused) audioNode.disconnect();
-  $: if (node === $timeline.curr && $player.isPaused && lastPauseState === "playing") {
-    lastPauseState = "paused";
-    pauseTimestamp = metadata.hasStarted ? audioContext.currentTime : 0;
-  }
+  // $: if (node === $timeline.current && video) !$player.isPaused ? video.play() : video.pause();
 
-  $: if (metadata.runtime >= metadata.duration - metadata.offsets[1] - metadata.offsets[0] && node.uuid == $timeline.curr?.uuid) {
-    console.log(`${node.metadata.name} ended: ${metadata.runtime} >= ${metadata.duration - metadata.offsets[1] - metadata.offsets[0]}`);
-    audioNode && audioNode.disconnect();
-    if (node.uuid === $timeline.clips.tail?.uuid) $player.isPaused = true;
-    else $timeline.curr = node.next;
-    metadata.hasEnded = true;
-  }
+  // TODO: test
+  $: if (node === $timeline.current && video) video.style.rotate = `${30}deg`;
+  $: if (node !== $timeline.current && video) video.style.rotate = `${0}deg`;
+  // $: if (audioNode && $player.isPaused) audioNode.disconnect();
+  // $: if (node === $timeline.current && $player.isPaused && lastPauseState === "playing") {
+  //   lastPauseState = "paused";
+  //   pauseTimestamp = metadata.hasStarted ? audioContext.currentTime : 0;
+  // }
 
-  const handlePlay = () => {
-    audioNode = audioContext.createBufferSource();
+  // $: if (metadata.runtime >= metadata.duration - metadata.offsets[1] - metadata.offsets[0] && node.uuid == $timeline.curr?.uuid) {
+  //   console.log(`${node.metadata.name} ended: ${metadata.runtime} >= ${metadata.duration - metadata.offsets[1] - metadata.offsets[0]}`);
+  //   audioNode && audioNode.disconnect();
+  //   if (node.uuid === $timeline.clips.tail?.uuid) $player.isPaused = true;
+  //   else $timeline.curr = node.next;
+  //   metadata.hasEnded = true;
+  // }
 
-    if (metadata.type === MediaType.VIDEO) {
-      const audioNodeBuffer = audioContext.createBuffer(metadata.audio.numberOfChannels, metadata.audio.length, metadata.audio.sampleRate);
-      for (let i = 0; i < metadata.audio.numberOfChannels; i++) audioNodeBuffer.copyToChannel(metadata.audio.getChannelData(i), i);
-      audioNode.buffer = audioNodeBuffer;
-    } else audioNode.buffer = audioContext.createBuffer(1, metadata.duration * 44100, 44100);
+  // const handlePlay = () => {
+  //   audioNode = audioContext.createBufferSource();
 
-    lastPauseState = "playing";
-    metadata.hasStarted = true;
+  //   if (metadata.type === MediaType.VIDEO) {
+  //     const audioNodeBuffer = audioContext.createBuffer(metadata.audio.numberOfChannels, metadata.audio.length, metadata.audio.sampleRate);
+  //     for (let i = 0; i < metadata.audio.numberOfChannels; i++) audioNodeBuffer.copyToChannel(metadata.audio.getChannelData(i), i);
+  //     audioNode.buffer = audioNodeBuffer;
+  //   } else audioNode.buffer = audioContext.createBuffer(1, metadata.duration * 44100, 44100);
 
-    let startOffset = metadata.offsets[0] + metadata.runtime;
-    if (!metadata.hasStarted) metadata.initialTimestamp = audioContext.currentTime;
-    else metadata.pauseAccumulator += audioContext.currentTime - pauseTimestamp - metadata.initialTimestamp;
+  //   lastPauseState = "playing";
+  //   metadata.hasStarted = true;
 
-    audioNode.connect(audioContext.destination);
-    audioNode.start(0, startOffset, metadata.duration - metadata.offsets[1] - startOffset);
+  //   let startOffset = metadata.offsets[0] + metadata.runtime;
+  //   if (!metadata.hasStarted) metadata.initialTimestamp = audioContext.currentTime;
+  //   else metadata.pauseAccumulator += audioContext.currentTime - pauseTimestamp - metadata.initialTimestamp;
 
-    console.log(`playing ${node.metadata.name} at ${startOffset} for ${metadata.duration - metadata.offsets[1] - startOffset}`);
-  };
+  //   audioNode.connect(audioContext.destination);
+  //   audioNode.start(0, startOffset, metadata.duration - metadata.offsets[1] - startOffset);
 
-  onMount(() => {
-    if (!video && !image) throw new Error("No buffer to mount.");
-    $timeline.buffers.set(node.uuid, video || image);
+  //   console.log(`playing ${node.metadata.name} at ${startOffset} for ${metadata.duration - metadata.offsets[1] - startOffset}`);
+  // };
 
-    node.metadata.setMediaTime = (time: number) => {
-      if (video) video.currentTime = time;
-      audioNode?.disconnect();
-      pauseTimestamp = 0;
-    };
-  });
+  // onMount(() => {
+  //   if (!video && !image) throw new Error("No buffer to mount.");
+  //   $timeline.buffers.set(node.uuid, video || image);
+
+  //   node.metadata.setMediaTime = (time: number) => {
+  //     if (video) video.currentTime = time;
+  //     audioNode?.disconnect();
+  //     pauseTimestamp = 0;
+  //   };
+  // });
 </script>
 
-<video muted class="pointer-events-none opacity-[0.000000001] fixed top-0 left-0" src={metadata.src} bind:this={video} on:play={handlePlay} />
+<!-- on:play={handlePlay} -->
+<video muted class="pointer-events-none opacity-[0.000000001] fixed top-0 left-0" src={node.src} bind:this={video} />
