e8c0e8ff357b43ebeaee36e455fdbc17fb797b75
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-05-26 02:54:40 +0100

    fix image timeline render

--- src/lib/components/mediaPreview/MediaVideoPreview.svelte
+++ src/lib/components/mediaPreview/MediaVideoPreview.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
   import { mediaPool } from "$lib/stores";
+  import { isImageMedia, isVideoMedia } from "../util/helpers";
 
   export let mediaUUID: string;
   export let isTimelineElement: boolean = false;
 
-  const media = $mediaPool.media.find((media) => media.uuid === mediaUUID) as App.VideoMedia;
+  const media = $mediaPool.media.find((media) => media.uuid === mediaUUID) as App.VideoMedia | App.ImageMedia;
 </script>
 
-<div class="relative w-full {isTimelineElement ? 'h-1/2' : 'h-full'} flex rounded-md overflow-clip">
-  {#each media.metadata.thumbnails as thumbnail}
-    <div class="h-full aspect-video bg-no-repeat bg-center bg-black" style="background-image:url({thumbnail}); background-size: auto 100%;" />
-  {/each}
+<div class="relative w-full h-full flex rounded-md overflow-clip">
+  {#if isVideoMedia(media)}
+    {#each media.metadata.thumbnails as thumbnail}
+      <div class="h-full aspect-video bg-no-repeat bg-center bg-black" style="background-image:url({thumbnail}); background-size: auto 100%;" />
+    {/each}
+  {:else}
+    <div class="h-full aspect-video bg-no-repeat bg-center bg-black" style="background-image:url({media.src}); background-size: auto 100%;" />
+  {/if}
 </div>
