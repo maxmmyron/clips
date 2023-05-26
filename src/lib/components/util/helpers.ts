64a3e0b8bfc151a2aa352b1ce9fe86f1d2fa5ec9
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-05-26 17:49:07 +0100

    implement node helpers

--- src/lib/components/util/helpers.ts
+++ src/lib/components/util/helpers.ts
@@ -9,3 +9,15 @@ export function isAudioMedia(media: App.VideoMedia | App.ImageMedia | App.AudioM
 export function isImageMedia(media: App.VideoMedia | App.ImageMedia | App.AudioMedia): media is App.ImageMedia {
   return media.type === "image";
 }
+
+export function isNodeVideoMedia(media: App.Node<"video" | "image" | "audio">): media is App.Node<"video"> {
+  return media.type === "video";
+}
+
+export function isNodeAudioMedia(media: App.Node<"video" | "image" | "audio">): media is App.Node<"audio"> {
+  return media.type === "audio";
+}
+
+export function isNodeImageMedia(media: App.Node<"video" | "image" | "audio">): media is App.Node<"image"> {
+  return media.type === "image";
+}
