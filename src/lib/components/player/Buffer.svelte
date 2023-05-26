b8bc47d47b8584d85ccaa66f46672eca21986200
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-05-26 04:33:26 +0100

    reimplement buffer (need to get video working with currTime)

--- src/lib/components/player/Buffer.svelte
+++ src/lib/components/player/Buffer.svelte
@@ -1,20 +1,29 @@
 <script lang="ts">
   import { timeline } from "$lib/stores";
+  import { onMount } from "svelte";
 
   // TODO: fix this so we only accept video and image buffers.
   export let nodeUUID: string, audioContext: AudioContext;
 
-  const node = $timeline.timeline.getByUUID(nodeUUID) as App.Node;
+  const node = $timeline.timeline.getByUUID(nodeUUID) as App.Node<"video" | "image">;
   const metadata = node.metadata;
 
-  let video: HTMLVideoElement, image: HTMLImageElement, audioNode: AudioBufferSourceNode | undefined;
+  let buffer: HTMLVideoElement | HTMLImageElement;
+
+  onMount(() => {
+    $timeline.sources.set(nodeUUID, { source: buffer, type: node.type });
+  });
 
   /**
    * The audioContext timestamp when the player was paused.
    */
-  let pauseTimestamp = 0;
-  let lastPauseState: "paused" | "playing" = "paused";
+  // let pauseTimestamp = 0;
+  // let lastPauseState: "paused" | "playing" = "paused";
 </script>
 
 <!-- on:play={handlePlay} -->
-<video muted class="pointer-events-none opacity-[0.000000001] fixed top-0 left-0" src={node.src} bind:this={video} />
+{#if node.type === "video"}
+  <video muted class="pointer-events-none opacity-[0.000000001] fixed top-0 left-0" src={node.src} bind:this={buffer} />
+{:else}
+  <img class="pointer-events-none opacity-[0.000000001] fixed top-0 left-0" src={node.src} bind:this={buffer} alt="" />
+{/if}
