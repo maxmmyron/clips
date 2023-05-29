6a47ea399303327b418909101abdf7226f83b50c
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-05-29 23:57:47 +0100

    add mimeType object, reader async loader, magic number parser

--- src/lib/MIMEExtractor.ts
+++ src/lib/MIMEExtractor.ts
@@ -1,15 +1,86 @@
-export function logFewBytes(file: File) {
-    const r = new FileReader();
-    r.onloadend = () => console.log(Array.from(new Uint8Array(r.result as ArrayBuffer).slice(0, 4)).map(b => b.toString(16).padStart(2, "0")).join(""));
-    r.readAsArrayBuffer(file.slice(0, 8));
+const mimeTypes: {[key: string]: {offset: number, magic: string[]}} = {
+  // *****************
+  // image
+  // *****************
+
+  // PNG
+  "image/png": {
+    offset: 0,
+    magic: ["89504e47"]
+  },
+
+  // JPG
+  "image/jpeg": {
+    offset: 0,
+    // TODO: confirm mime types
+    magic: ["ffd8ffdb", "ffd8ffe0", "ffd8ffe1", "ffd8ffe2", "ffd8ffe3", "ffd8ffe8"]
+  },
+
+  // *****************
+  // audio
+  // *****************
+
+  // MP3
+  "audio/mpeg": {
+    offset: 0,
+    magic: ["fffb", "fff3", "fff2", "494433"]
+  },
+
+  // ogg
+  "audio/ogg": {
+    offset: 0,
+    magic: ["4f676753"],
+  },
+
+
+  // *****************
+  // video
+  // *****************
+
+  // MP4
+  "video/mp4": {
+    offset: 4,
+    magic: ["6674797069736F6D"],
+  },
+
+  "video/m4v": {
+    offset: 4,
+    magic: ["667479706D703432"],
+  }
+
 }
 
-// image
+const awaitReader = async (file: File, offset: number, length: number) => {
+  const reader = new FileReader();
 
-// png magic numbers: 89 50 4E 47
-// jpeg magic numbers: FF D8 FF (E0/E1/E2/E3/E8)
+  return new Promise((resolve, reject) => {
+    reader.onloadend = () => {
+      const bytes = Array.from(new Uint8Array(reader.result as ArrayBuffer)).map(b => b.toString(16).padStart(2, "0")).join("");
+      resolve(bytes);
 
-// video
+      // destroy reader
+      reader.onloadend = null;
+      reader.onerror = null;
+    }
 
+    reader.onerror = reject;
 
-// audio
+    reader.readAsArrayBuffer(file.slice(offset, offset + length));
+  });
+}
+
+export async function logFewBytes (file: File): Promise<string> {
+    for (const [mimeType, {offset, magic}] of Object.entries(mimeTypes)) {
+      for (const m of magic) {
+        const bytes = await awaitReader(file, offset, m.length / 2);
+        console.log(`checking ${mimeType} with magic ${m} against ${bytes}...`);
+
+        if (bytes === m.toLowerCase()) {
+          console.log("found", mimeType, m);
+          return mimeType;
+        }
+      }
+    }
+
+    return "unknown";
+};
