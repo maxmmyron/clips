5d17e7bb81f7a80778bc432bdeb9aae4f9b99bb4
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-06-04 04:04:14 +0100

    update preview design

--- src/lib/components/mediaPreview/MediaPoolPreview.svelte
+++ src/lib/components/mediaPreview/MediaPoolPreview.svelte
@@ -53,20 +53,33 @@
     </div>
   </div>
 {:else}
-  <button
-    bind:this={mediaPreview}
-    on:click|capture|stopPropagation={handleClick}
-    class="relative flex flex-col outline-2 outline-blue-600 w-48 rounded-md overflow-clip aspect-video"
-    class:outline={isSelected}
-    on:mousedown={handleDragStart}
-  >
-    <slot />
-    <div class="absolute py-1 px-2 bg-[rgba(0,0,0,0.4)] backdrop-blur-sm w-full bottom-0 overflow-clip" bind:clientWidth={containerWidth}>
-      <p style="--overflow-scroll-pos: {nameOffset}px" class="relative text-white m-0 w-max {shouldNameAnimate && 'animate'}" bind:clientWidth={nameWidth}>
-        {media.metadata.title}
-      </p>
+  <div class="group">
+    <button
+      bind:this={mediaPreview}
+      on:click|capture|stopPropagation={handleClick}
+      class="w-48 aspect-video outline-2 outline-blue-600 rounded-md overflow-clip mb-2"
+      class:outline={isSelected}
+      on:mousedown={handleDragStart}
+    >
+      <slot />
+    </button>
+    <div class="w-48 overflow-clip grid grid-rows-1 grid-cols-[24px,auto,24px] gap-1 items-center">
+      <img alt="" src="/img/{media.type}-icon_dark.svg" class="w-4 h-4" />
+      <div class="overflow-clip" bind:clientWidth={containerWidth}>
+        <p
+          style="--overflow-scroll-pos: {nameOffset}px"
+          class="relative text-neutral-300 font-mono m-0 w-max {shouldNameAnimate && 'animate'}"
+          bind:clientWidth={nameWidth}
+        >
+          {media.metadata.title}
+        </p>
+      </div>
+
+      <button class="w-6 h-6 p-1 opacity-0 group-hover:opacity-100 hover:brightness-200 transition-all">
+        <img alt="Open Settings" src="/img/settings-icon_dark.svg" class="w-4 h-4" />
+      </button>
     </div>
-  </button>
+  </div>
 {/if}
 
 <style>
