03d4f03db47c86391b2500f334fbb04c5cd21b1d
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-06-05 17:55:40 +0100

    abstract out controls

---
+++ src/lib/components/player/Controls.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { player, timeline } from "$lib/stores";
+
+  function setPlayerTime(front: boolean = true): any {
+    console.log(`skipping to ${front ? "front" : "back"}`);
+    $player.isPaused = true;
+
+    $timeline.runtime = front ? 0 : $timeline.duration;
+    $timeline.current = front ? $timeline.timeline.head : $timeline.timeline.tail;
+  }
+</script>
+
+<div class="flex justify-center gap-4">
+  <button class="text-white border-2 border-neutral-800 px-3 py-1 disabled:opacity-50 disabled:cursor-not-allowed" on:click={() => setPlayerTime()}>
+    ⏪
+  </button>
+  <button
+    class="text-white border-2 border-neutral-800 px-3 py-1 disabled:opacity-50 disabled:cursor-not-allowed"
+    on:click={() => ($player.isPaused = !$player.isPaused)}
+  >
+    {$player.isPaused ? "▶️" : "⏸️"}
+  </button>
+  <button class="text-white border-2 border-neutral-800 px-3 py-1 disabled:opacity-50 disabled:cursor-not-allowed" on:click={() => setPlayerTime(false)}>
+    ⏩
+  </button>
+</div>
--- src/lib/components/player/EditorPlayer.svelte
+++ src/lib/components/player/EditorPlayer.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-  import { timeline, player } from "$lib/stores";
+  import { timeline } from "$lib/stores";
   import { Canvas, Layer, t, type Render } from "svelte-canvas";
   import Buffer from "./Buffer.svelte";
 
-  export let width = 640,
-    height = 480;
+  let width: number, height: number;
 
   let audioContext: AudioContext = new AudioContext();
 
@@ -12,7 +11,11 @@
   $: render = ({ context, width, height }) => {
     $t;
 
-    if (!$timeline.current) return;
+    if (!$timeline.current) {
+      context.fillStyle = "black";
+      context.fillRect(0, 0, width, height);
+      return;
+    }
 
     if ($timeline.current.type === "audio") {
       context.fillStyle = "black";
@@ -35,16 +38,6 @@
 
     context.drawImage(src.source, 0, 0, bufferWidth, bufferHeight, ...mediaPosition, mediaSize.width, mediaSize.height);
   };
-
-  function setPlayerTime(front: boolean = true): any {
-    console.log(`skipping to ${front ? "front" : "back"}`);
-    $player.isPaused = true;
-
-    $timeline.runtime = front ? 0 : $timeline.duration;
-    $timeline.current = front ? $timeline.timeline.head : $timeline.timeline.tail;
-  }
-
-  const timingRules: ((arg0: number) => number)[] = [(runtime) => runtime / 3600, (runtime) => (runtime % 3600) / 60, (runtime) => runtime % 60];
 </script>
 
 <!-- TODO: this *will* filter out audio nodes, but a type error is thrown unless the Buffer.svelte element accepts all node types. Need to investigate and fix (shoddy types) -->
@@ -53,41 +46,8 @@
   <Buffer nodeUUID={node.uuid} {audioContext} />
 {/each}
 
-<div class="w-full h-full flex justify-center items-center">
-  {#if $timeline.timeline.length}
-    <Canvas {width} {height}>
-      <Layer {render} />
-    </Canvas>
-  {:else}
-    <p class="text-neutral-400 font-mono">no clips in timeline （＞人＜；）</p>
-  {/if}
-</div>
-
-<div class="flex justify-end">
-  {#each timingRules as r, i}
-    <p class="text-neutral-200 font-mono">
-      {Math.floor(r($timeline.runtime)).toString().padStart(2, "0")}:
-    </p>
-  {/each}
-  <p class="text-neutral-200 font-mono">
-    <!-- Loving the lack of configurability for chained call new lines!!!! Super happy this line of code is ripped apart onto three separate lines!!!!!! https://github.com/prettier/prettier/issues/1565 -->
-    {Math.round(($timeline.runtime % 1) * 1000)
-      .toString()
-      .padStart(3, "0")}
-  </p>
-</div>
-
-<div class="w-full flex justify-center gap-4">
-  <button class="text-white border-2 border-neutral-800 px-3 py-1 disabled:opacity-50 disabled:cursor-not-allowed" on:click={() => setPlayerTime()}>
-    ⏪
-  </button>
-  <button
-    class="text-white border-2 border-neutral-800 px-3 py-1 disabled:opacity-50 disabled:cursor-not-allowed"
-    on:click={() => ($player.isPaused = !$player.isPaused)}
-  >
-    {$player.isPaused ? "▶️" : "⏸️"}
-  </button>
-  <button class="text-white border-2 border-neutral-800 px-3 py-1 disabled:opacity-50 disabled:cursor-not-allowed" on:click={() => setPlayerTime(false)}>
-    ⏩
-  </button>
+<div class="max-w-full aspect-video" bind:clientWidth={width} bind:clientHeight={height}>
+  <Canvas {width} {height}>
+    <Layer {render} />
+  </Canvas>
 </div>
