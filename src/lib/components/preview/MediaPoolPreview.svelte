802e172fdb13829d6614ba06cb4db4c7629b0bb1
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-06-08 20:48:36 +0100

    update preloader media design

--- src/lib/components/preview/MediaPoolPreview.svelte
+++ src/lib/components/preview/MediaPoolPreview.svelte
@@ -43,41 +43,53 @@
 </script>
 
 {#if media === null || hasResolved === false}
-  <div class="relative flex flex-col w-48 rounded-md overflow-clip aspect-video">
-    <p class="text-gray-400">{currentLoadState}</p>
-
-    <div class="absolute py-1 px-2 bg-[rgba(0,0,0,0.4)] backdrop-blur-sm w-full bottom-0 overflow-clip" bind:clientWidth={containerWidth}>
-      <p style="--overflow-scroll-pos: {nameOffset}px" class="relative text-white m-0 w-max {shouldNameAnimate && 'animate'}" bind:clientWidth={nameWidth}>
-        {name}
-      </p>
+  <div class="rounded-lg p-1">
+    <div class="w-48 aspect-video rounded-md bg-gray-500 mb-2 flex justify-center items-center">
+      <p class="text-gray-200">{currentLoadState}</p>
     </div>
-  </div>
-{:else}
-  <div class="group">
-    <button
-      bind:this={mediaPreview}
-      on:click|capture|stopPropagation={handleClick}
-      class="w-48 aspect-video outline-2 outline-blue-600 rounded-md overflow-clip mb-2"
-      class:outline={isSelected}
-      on:mousedown={handleDragStart}
+    <div
+      class="w-48 overflow-clip grid grid-rows-1 grid-cols-[24px,auto] gap-1 items-center after:content-[''] after:w-5 after:h-full after:absolute after:top-0 after:right-0 after:bg-gradient-to-l after:from-{isSelected
+        ? 'gray-800'
+        : 'neutral-900'} after:to-transparent"
     >
-      <slot />
-    </button>
-    <div class="w-48 overflow-clip grid grid-rows-1 grid-cols-[24px,auto,24px] gap-1 items-center">
-      <img alt="" src="/icons/{media.type}_dark.svg" class="w-4 h-4" />
-      <div class="overflow-clip" bind:clientWidth={containerWidth}>
+      <div class="overflow-clip col-start-2" bind:clientWidth={containerWidth}>
         <p
           style="--overflow-scroll-pos: {nameOffset}px"
           class="relative text-neutral-200 font-mono m-0 w-max {shouldNameAnimate && 'animate'}"
           bind:clientWidth={nameWidth}
         >
-          {media.metadata.title}
+          {name}
         </p>
       </div>
-
-      <button class="w-6 h-6 p-1 opacity-0 group-hover:opacity-100 hover:brightness-200 transition-all">
-        <img alt="Open Settings" src="/icons/settings_dark.svg" class="w-4 h-4" />
-      </button>
+    </div>
+  </div>
+{:else}
+  <div class="rounded-lg p-1 transition-colors" class:bg-gray-800={isSelected}>
+    <button
+      bind:this={mediaPreview}
+      on:click|capture|stopPropagation={handleClick}
+      class="w-48 aspect-video rounded-md overflow-clip mb-2"
+      on:mousedown={handleDragStart}
+    >
+      <slot />
+    </button>
+    <div class="relative">
+      <div
+        class="w-48 overflow-clip grid grid-rows-1 grid-cols-[24px,auto] gap-1 items-center after:content-[''] after:w-5 after:h-full after:absolute after:top-0 after:right-0 after:bg-gradient-to-l after:from-{isSelected
+          ? 'gray-800'
+          : 'neutral-900'} after:to-transparent"
+      >
+        <img alt="" src="/icons/{media.type}_dark.svg" class="w-4 h-4" />
+        <div class="overflow-clip" bind:clientWidth={containerWidth}>
+          <p
+            style="--overflow-scroll-pos: {nameOffset}px"
+            class="relative text-neutral-200 font-mono m-0 w-max {shouldNameAnimate && 'animate'}"
+            bind:clientWidth={nameWidth}
+          >
+            {media.metadata.title}
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 {/if}
