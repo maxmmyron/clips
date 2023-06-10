e11aacf3d918c5310941ecc78fb7cae9a8238209
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-06-10 21:22:56 +0100

    improve ghost layout

--- src/lib/components/util/Ghost.svelte
+++ src/lib/components/util/Ghost.svelte
@@ -3,7 +3,7 @@
   import { fade } from "svelte/transition";
   import Marquee from "./Marquee.svelte";
 
-  $: media = $mediaPool.media.find((media) => media.uuid === $studio.draggable.mediaUUID);
+  $: media = $mediaPool.media.find((m) => m.uuid === $studio.draggable.mediaUUID);
   $: pos = $studio.draggable.ghost.pos;
   $: size = $studio.draggable.ghost.size;
   $: region = $studio.draggable.current.region;
@@ -11,16 +11,17 @@
 
 {#if media && $studio.draggable.event !== "start"}
   <div
-    class="absolute z-10 w-max-[12rem] py-1 px-3 bg-neutral-800 border-2 border-neutral-700 {region === 'timeline'
+    class="absolute z-10 {region === null && 'max-w-[12rem]'} py-1 px-3 bg-neutral-800 border-2 border-neutral-700 {region === 'timeline'
       ? 'rounded-md'
-      : 'rounded-full'} transition-none pointer-events-none flex items-center gap-4 {region !== 'timeline' && '-translate-y-[50%] -translate-x-[25%]'}"
+      : 'rounded-full'} overflow-clip transition-none pointer-events-none flex items-center gap-4 {region !== 'timeline' &&
+      '-translate-y-[50%] -translate-x-[25%]'}"
     style="left: {$pos.x}px; top: {$pos.y}px; {region === 'timeline' && `width: ${$size.width}px;`} height: {$size.height}px;"
     transition:fade={{ duration: 150 }}
   >
     {#if region === null}
       <img alt="" src="/icons/{media.type}_dark.svg" class="w-4 h-4" />
-      <div class="overflow-clip">
-        <Marquee>{media.metadata.title}</Marquee>
+      <div class="overflow-clip flex-1" style="width: calc(100% - 3rem);">
+        <Marquee primaryColor={"neutral-800"}>{media.metadata.title}</Marquee>
       </div>
     {/if}
   </div>
