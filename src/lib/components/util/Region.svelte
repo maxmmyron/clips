ff52cfcd95583b3d8ff9e20ff826c1e4db4a2d89
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-06-12 16:49:43 +0100

    improve shading

--- src/lib/components/util/Region.svelte
+++ src/lib/components/util/Region.svelte
@@ -5,7 +5,7 @@
   export let innerClass = "";
 </script>
 
-<div class="w-full h-full bg-gradient-to-b from-neutral-900 to-neutral-950 to-90% p-0.5 rounded-md {clazz}">
+<div class="w-full h-full bg-gradient-to-b from-neutral-800 to-neutral-900 to-90% p-0.5 rounded-md {clazz}">
   <div class="w-full h-full bg-neutral-900 rounded-[.3125rem] shadow-inner-lg {innerClass}">
     <slot />
   </div>
