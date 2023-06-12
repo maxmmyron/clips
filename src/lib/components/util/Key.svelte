c63884da2f8247d2b4855ffc462bcf1e6fdd6535
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-06-12 04:24:13 +0100

    abstract key logic

---
+++ src/lib/components/util/Key.svelte
@@ -0,0 +1,8 @@
+<div
+  class="flex justify-center items-center
+  min-w-[1.5rem] h-6 px-[0.375rem]
+  rounded-md border-[1px] border-neutral-700
+  bg-gradient-to-b from-zinc-800 to-zinc-950"
+>
+  <span class="m-0 text-neutral-300 text-sm font-mono"><slot /></span>
+</div>
