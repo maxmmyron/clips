1effe09ab9be751bb6df85b47315e2f4ec6426b5
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-06-17 04:38:41 +0100

    todo: fix wrapping issue

--- src/lib/components/timeline/Ticks.svelte
+++ src/lib/components/timeline/Ticks.svelte
@@ -6,19 +6,37 @@
 
   let canvas: HTMLCanvasElement;
 
+  let lastScrollX = 0;
+  let lastZoom = 0;
+
   $: ctx = canvas?.getContext("2d") ?? null;
   $: if (ctx) {
+    if (lastScrollX !== scrollX || lastZoom !== $timeline.zoomScale) {
+      lastScrollX = scrollX;
+      lastZoom = $timeline.zoomScale;
+      draw(ctx);
+    }
+  }
+
+  const draw = (ctx: CanvasRenderingContext2D) => {
     ctx.strokeStyle = "white";
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    let wrap = Math.ceil(scrollX / $timeline.zoomScale);
+
     for (let i = 0; i < numTicks; i++) {
-      const x = i * $timeline.zoomScale - scrollX;
+      let x = ((i - scrollX / $timeline.zoomScale) * $timeline.zoomScale) % timelineWidth;
+      if (x < 0) x += timelineWidth;
+
       ctx.beginPath();
       ctx.moveTo(x, 0);
       ctx.lineTo(x, 48);
       ctx.stroke();
+
+      ctx.font = "16px Arial";
+      ctx.strokeText(`${i + wrap}`, x + 4, 16);
     }
-  }
+  };
 
   $: numTicks = Math.ceil(timelineWidth / $timeline.zoomScale);
 </script>
