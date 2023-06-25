3fc91eb50e91e7894582345208e75f662e7f2aa0
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-06-24 22:03:49 -0400

    update ticks layout

--- src/lib/components/timeline/Ticks.svelte
+++ src/lib/components/timeline/Ticks.svelte
@@ -11,13 +11,13 @@
   $: secondScale = Math.floor(($timeline.zoomScale - 50) / 10);
 </script>
 
-<div class="absolute w-full h-6 top-0 overflow-clip transform" translate="">
+<div class="absolute w-full h-6 top-5 overflow-x-clip transform" translate="">
   <div class="w-[120%] h-full flex transform" bind:clientWidth={width} style="--tw-translate-x:-{scrollX % (width / 6)}px;">
     {#each { length: 6 } as _, i}
-      <div class="relative flex w-1/5 h-full border-l-2 border-l-white">
-        <p class="absolute text-white">{(i + offset) * 2 ** -secondScale}</p>
+      <div class="relative flex w-1/5 h-full border-l-2 border-l-neutral-400">
+        <p class="absolute text-neutral-300 -left-0.5 -top-6">{(i + offset) * 2 ** -secondScale}</p>
         {#each tickHeights as h}
-          <div class="w-full border-l-2 border-l-white" style="height: {h * 12}px" />
+          <div class="w-full border-l-2 border-l-neutral-400" style="height: {h * 12}px" />
         {/each}
       </div>
     {/each}
