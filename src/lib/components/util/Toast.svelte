3831634e73491214ecac3993983674a848b31afb
Max <41480159+maxmmyron@users.noreply.github.com>
2023-06-26 18:36:23 -0400

    try removing transitions for now

--- src/lib/components/util/Toast.svelte
+++ src/lib/components/util/Toast.svelte
@@ -30,7 +30,7 @@
     };
 </script>
 
-<div class="w-sm min-h-[4rem] bg-slate-800 border-2 border-bg-slate-700 p-4" in:fly={{x: "100%"}} out:fly={{x:"100%"}}>
+<div class="w-sm min-h-[4rem] bg-slate-800 border-2 border-bg-slate-700 p-4">
     <div class="w-full h-5 flex justify-between items-center mb-4">
         <!-- This doesn't have to be in an img wrapper el because toast level color not light/dark dependent -->
         <img src="/static/{toast.level}.svg" alt="{toast.level}" />
