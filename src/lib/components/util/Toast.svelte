22da50c67b3db886d2c3dcd5537d601464ed68a7
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-06-27 10:25:52 -0400

    reimplement transition

--- src/lib/components/util/Toast.svelte
+++ src/lib/components/util/Toast.svelte
@@ -4,22 +4,23 @@
 
     export let toast: App.Toast;
 
-    // const removeToast = () => {
-    //     // remove timeout before expiration
-    //     clearTimeout(toast.timeoutID);
+    const removeToast = () => {
+        // remove timeout before expiration
+        clearTimeout(toast.timeoutID);
 
-    //     const idx = $toasts.findIndex((t) => t.uuid === toast.uuid);
-    //     $toasts.splice(idx, 1);
-    // };
+        const idx = $toasts.findIndex((t) => t.uuid === toast.uuid);
+        $toasts.splice(idx, 1);
+        $toasts = [...$toasts];
+    };
 </script>
 
-<div class="w-sm min-h-[4rem] bg-slate-800 border-2 border-bg-slate-700 p-4">
+<div class="w-sm min-h-[4rem] bg-slate-800 border-2 border-bg-slate-700 p-4" in:fly={{ x: "100%" }} out:fly={{ x: "100%" }}>
     <div class="w-full h-5 flex justify-between items-center mb-4">
         <!-- This doesn't have to be in an img wrapper el because toast level color not light/dark dependent -->
-        <!-- <img src="/static/{toast.level}.svg" alt={toast.level} />
+        <img src="/static/{toast.level}.svg" alt={toast.level} />
         <button on:click={removeToast}>
             <img src="/static/close.svg" alt="Close Toast" class="w-5 h-5" />
-        </button> -->
+        </button>
     </div>
     <p class="text-white">{toast.message}</p>
 </div>
