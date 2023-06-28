2794cb138ebfb7b4b1ca3f26b5414fb4be43b665
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-06-27 21:38:05 -0400

    fix filter

--- src/lib/util/toastManager.ts
+++ src/lib/util/toastManager.ts
@@ -3,5 +3,5 @@ import { toasts } from "$lib/stores"
 
 export const addToast = (level: "info" | "warning" | "error", message: string, duration = 7000) => {
   const uuid = uuidv4()
-  toasts.update(toasts => [...toasts, { uuid, level, message, timeoutID: setTimeout(() => toasts = toasts.filter(toast => toast.uuid !== uuid), duration) }])
+  toasts.update(toastsArr => [...toastsArr, { uuid, level, message, timeoutID: setTimeout(() => toasts.update(toastsArr => toastsArr.filter(toast => toast.uuid !== uuid)), duration) }])
 }
