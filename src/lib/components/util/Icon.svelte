770af6530ba4f96528d08ad6fcf1a3232f24bc96
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-06-30 11:10:50 -0400

    fix icons not loading in chrome

--- src/lib/components/util/Icon.svelte
+++ src/lib/components/util/Icon.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
   export let src: string;
 
   // TODO: add alt tag that works with screen readers
@@ -6,4 +8,13 @@
   export let fill: string = "";
 </script>
 
-<div class="w-4 h-4 {fill ? fill : 'bg-neutral-200'}" style="mask: url({src}) no-repeat center / contain" />
+<div class="w-4 h-4 {fill ? fill : 'bg-neutral-200'}" style="--src: url('{src}')" />
+
+<style>
+  div {
+    mask-image: var(--src);
+    mask-repeat: no-repeat;
+    mask-position: center;
+    mask-size: contain;
+  }
+</style>
