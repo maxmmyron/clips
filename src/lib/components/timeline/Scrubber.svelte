034de9e3a867810fb756d9d9e0ebfcf8feadc858
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-07-04 23:29:12 -0400

    simplify scrubber

--- src/lib/components/timeline/Scrubber.svelte
+++ src/lib/components/timeline/Scrubber.svelte
@@ -2,16 +2,15 @@
   import { timeline } from "$lib/stores";
 
   export let scrollX: number;
-  export let timelineSecondWidth: number;
 
-  $: secondScale = 2 ** (5 - $timeline.zoomScale);
-  $: scrubberPos = $timeline.runtime * (timelineSecondWidth / secondScale) - scrollX;
+  $: secondWidth = 2 ** $timeline.zoomScale;
+  $: scrubberPos = $timeline.runtime * secondWidth - scrollX;
 </script>
 
 <div
   id="scrubber"
   style="left: {scrubberPos}px"
-  class="absolute w-0.5 h-3/4 top-1/2 transform -translate-y-1/2 bg-neutral-300 rounded-full pointer-events-none"
+  class="absolute w-0.5 h-full top-1/2 transform -translate-y-1/2 bg-neutral-300 rounded-full pointer-events-none"
 />
 
 <style>
