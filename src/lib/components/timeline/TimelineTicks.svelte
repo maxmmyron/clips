8eda8cfd6b263508eba878d45517d7f46678f996
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-07-05 09:47:09 -0400

    fix sizing

--- src/lib/components/timeline/TimelineTicks.svelte
+++ src/lib/components/timeline/TimelineTicks.svelte
@@ -6,13 +6,13 @@
   let width: number;
   let canCalcRuntime = false,
     previousPauseState = false;
-  const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
 
   $: secondWidth = 2 ** $timeline.zoomScale;
   $: offset = Math.floor(scrollX / (width / numSections));
   $: numSections = Math.ceil(Math.max(3, Math.min(15, width / secondWidth)));
 
-  const calcRuntime = (e: MouseEvent) => canCalcRuntime && ($timeline.runtime = (e.clientX - 1.25 * rem + scrollX) / secondWidth);
+  // FIXME: 22 is magic padding number; this will not work in all cases due to padding based off of rem
+  const calcRuntime = (e: MouseEvent) => canCalcRuntime && ($timeline.runtime = (e.clientX - 22 + scrollX) / secondWidth);
 
   const handleMouseDown = (e: MouseEvent) => {
     previousPauseState = $player.isPaused;
@@ -28,16 +28,16 @@
 </script>
 
 <div
-  class="min-w-full h-12 flex select-none"
-  style="width: {(numSections + 1) * secondWidth}px; transform: translateX({-scrollX % (width / numSections)}px);"
+  class="min-w-full h-12 flex select-none transition-none"
+  style="width: {numSections * secondWidth}px; transform: translateX({-scrollX % (width / numSections)}px);"
   bind:clientWidth={width}
   on:mousemove={calcRuntime}
   on:mousedown={handleMouseDown}
   on:mouseup={handleMouseUp}
 >
   {#each { length: numSections + 1 } as _, i}
-    <div class="relative rounded-md bg-blue-200 h-full" style="width: {secondWidth}px; min-width: calc(100% / {numSections});">
-      {Math.floor((i + offset) * Math.max(1, width / numSections / secondWidth) * 100) / 100}
+    <div class="relative rounded-md h-full transition-none" style="width: {secondWidth}px; min-width: calc(100% / {numSections});">
+      <p class="absolute font-mono text-white">{Math.floor((i + offset) * Math.max(1, width / numSections / secondWidth) * 100) / 100}</p>
     </div>
   {/each}
 </div>
