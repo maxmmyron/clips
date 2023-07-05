549b8d04159d9de1694557fb1639ef352d9a6b04
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-07-05 17:49:01 -0400

    add drag threshold, improve guards, reimplement select/delete

--- src/lib/components/timeline/Timeline.svelte
+++ src/lib/components/timeline/Timeline.svelte
@@ -13,24 +13,23 @@
 
   const setupDrag = (e: MouseEvent) => {
     if (e.button !== 0) return;
-
     $draggable.event = "start";
     $draggable.origin = { pos: { x: e.clientX, y: e.clientY }, region: "timeline" };
   };
 
   const handleDrag = (e: MouseEvent) => {
-    if ($draggable.event !== "start") return;
-
+    if ($draggable.event !== "start" || !$draggable.origin) return;
+    if (Math.sqrt(Math.pow(e.clientX - $draggable.origin.pos.x, 2) + Math.pow(e.clientY - $draggable.origin.pos.y, 2)) < 15) return;
     $draggable.event = "drag";
   };
 
   const endDrag = (e: MouseEvent) => {
     console.log("end drag");
-    if ($draggable.event !== "drag" || !$draggable.media) return;
-
-    if ($draggable.origin?.region === "media_pool") {
+    if ($draggable.event !== "drag" || !$draggable.origin) return;
+    if ($draggable.origin.region === "timeline") calculateReorder();
+    else {
+      if (!$draggable.media) return;
       const media = $draggable.media;
-
       $timeline.clips.add({
         uuid: uuidv4(),
         mediaUUID: media.uuid,
@@ -45,11 +44,23 @@
         src: media.src,
         type: media.type,
       });
-
-      $timeline.clips = $timeline.clips;
     }
+    $timeline.clips = $timeline.clips;
+  };
+
+  const calculateReorder = () => {
+    console.log("reorder");
+  };
+
+  const handleKey = (e: KeyboardEvent & { currentTarget: EventTarget & Window }) => {
+    if (e.key !== "Delete" || selected.length === 0) return;
+    selected.forEach((uuid) => $timeline.clips.remove(uuid));
+    $timeline.clips = $timeline.clips;
+    selected = [];
   };
 </script>
+
+<svelte:window on:click={() => (selected = [])} on:keydown={handleKey} />
 
 <div
   class="overflow-x-auto flex-grow h-full flex"
