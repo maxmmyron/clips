50f0be9983b3fb87747cf503eeb5ba09e321384a
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-07-07 11:43:20 -0400

    why were these even functions lol

--- src/lib/components/media/browserParser.ts
+++ src/lib/components/media/browserParser.ts
@@ -1,16 +1,12 @@
 export async function assertBrowserSupportsContainer(mimeType: string): Promise<boolean> {
-    if(mimeType.includes("video") || mimeType.includes("audio")) return assertMediaContainerSupport(mimeType);
-    if(mimeType.includes("image")) return mimeType.includes("avif") ? await assertAVIFSupport(mimeType) : true;
+    if(mimeType.includes("video") || mimeType.includes("audio")) {
+        const canPlayMIME = document.createElement('video').canPlayType(mimeType);
+        return canPlayMIME === 'maybe' || canPlayMIME === 'probably';
+    }
+    if (mimeType.includes("image") && mimeType.includes("avif")) {
+        const avif = "data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUEAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAABYAAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAEAAAABAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgSAAAAAAABNjb2xybmNseAACAAIABoAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAAB5tZGF0EgAKBzgADlAgIGkyCR/wAABAAACvcA==";
+        return await fetch(avif).then(res => res.blob()).then(blob => createImageBitmap(blob).then(() => true)).catch(() => false);
+    }
+    if(mimeType.includes("image")) return true;
     return false;
-}
-
-const assertMediaContainerSupport = (mimeType: string) => {
-    const canPlayMIME = document.createElement('video').canPlayType(mimeType);
-    return canPlayMIME === 'maybe' || canPlayMIME === 'probably';
-}
-
-// AVIF support shaky; manually check support here
-const assertImageContainerSupport = async (mimeType: string) => {
-    const avif = "data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUEAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAABYAAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAEAAAABAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgSAAAAAAABNjb2xybmNseAACAAIABoAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAAB5tZGF0EgAKBzgADlAgIGkyCR/wAABAAACvcA==";
-    return await fetch(avif).then(res => res.blob()).then(blob => createImageBitmap(blob).then(() => true)).catch(() => false);
 }
