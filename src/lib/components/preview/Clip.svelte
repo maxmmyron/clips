c1ab46a23a907ed729cba081b44369cf40d1073d
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-07-08 23:53:33 -0400

    it was working most of the time

--- src/lib/components/preview/Clip.svelte
+++ src/lib/components/preview/Clip.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import { secondWidth, studio } from "$lib/stores";
+  import { media, secondWidth, studio } from "$lib/stores";
 
   export let clip: App.Clip;
   export let selected: string[];
 
   let mediaPreview: HTMLButtonElement;
-  let isMove = false;
-  let isResize = false;
+  let isMove = false, isResize = false;
+  let initialResizePosition = 0, initialOffset = 0;
   let resizeDirection: "left" | "right" = "left";
 
   /**
@@ -16,22 +16,25 @@
 
   $: isSelected = selected.includes(clip.uuid);
 
-  const handleMove = () => {
+  const handleMove = (e: MouseEvent) => {
     if(isMove) {
       clip.metadata.offset = ($studio.mouse.x - initialX) / $secondWidth;
       return
     }
 
-    if(isResize) {
+    if(isResize) {  
+      let offset;
       // if left, resize based on mouse position between 0 and metadata.runtime
       if(resizeDirection == "left") {
-        //clip.metadata.start = clip.metadata.start + Math.max(0, clip.metadata.start + ($studio.mouse.x - initialX)) / $secondWidth;
+        offset = initialOffset + (e.clientX - initialResizePosition) / $secondWidth;
+        clip.metadata.start = Math.max(0, offset);
         return;
       }
 
       // if right, resize based on mouse position between metadata.start and metadata.duration
       if(resizeDirection == "right") {
-        //clip.metadata.runtime = clip.metadata.runtime + Math.max(0, clip.metadata.runtime + ($studio.mouse.x - initialX)) / $secondWidth;
+        offset = initialOffset - (initialResizePosition - e.clientX) / $secondWidth;
+        clip.metadata.runtime = Math.min(offset, clip.metadata.duration);
         return;
       }
     }
@@ -45,6 +48,8 @@
   const setupMove = (e: MouseEvent) => {
     if(e.clientX - mediaPreview.getBoundingClientRect().x < 12) {
       isResize = true;
+      initialResizePosition = mediaPreview.getBoundingClientRect().left;
+      initialOffset = clip.metadata.start;
       resizeDirection = "left";
       initialX = e.clientX;
       return;
@@ -52,6 +57,8 @@
 
     if(mediaPreview.getBoundingClientRect().right - e.clientX < 12) {
       isResize = true;
+      initialResizePosition = mediaPreview.getBoundingClientRect().right;
+      initialOffset = clip.metadata.runtime;
       resizeDirection = "right";
       initialX = e.clientX;
       return;
@@ -71,9 +78,9 @@
 
 <button
   class="absolute h-12 bg-neutral-800 rounded-lg {isSelected ? "border-neutral-600" : "border-neutral-700/50"} border-2 transition-colors 
-  before:absolute before:w-3 before:h-full before:left-0 before:top-0 before:hover:cursor-ew-resize
-  after:absolute after:w-3 after:h-full after:right-0 after:top-0 after:hover:cursor-ew-resize"
-  style="width: {clip.metadata.runtime * $secondWidth}px; transform: translateX({clip.metadata.offset * $secondWidth}px);"
+  before:absolute before:w-4 before:h-full before:-left-1 before:top-0 before:hover:cursor-ew-resize
+  after:absolute after:w-4 after:h-full after:-right-1 after:top-0 after:hover:cursor-ew-resize"
+  style="width: {(clip.metadata.runtime - clip.metadata.start) * $secondWidth}px; transform: translateX({(clip.metadata.offset + clip.metadata.start) * $secondWidth}px);"
   on:click|capture|stopPropagation={handleClick}
   on:mousedown={setupMove}
   bind:this={mediaPreview}
