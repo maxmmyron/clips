c0a9fb63b7576f2609ff0dd899fc989480b1b3ce
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-08-09 09:55:52 -0400

    investigate perf issues (lies solely in .drawImage())

--- src/lib/components/player/Player.svelte
+++ src/lib/components/player/Player.svelte
@@ -4,24 +4,39 @@
 
   export let width: number, height: number;
 
-  const calcDimensions = (type: "video" | "image", buffer: HTMLVideoElement | HTMLImageElement) => {
+  const calcDimensions = (
+    type: "video" | "image",
+    buffer: HTMLVideoElement | HTMLImageElement
+  ) => {
     let sourceDim: [number, number] = [-1, -1];
 
-    if (type === "video") sourceDim = [(buffer as HTMLVideoElement).videoWidth, (buffer as HTMLVideoElement).videoHeight];
-    else if (type === "image") sourceDim = [(buffer as HTMLImageElement).naturalWidth, (buffer as HTMLImageElement).naturalHeight];
+    if (type === "video")
+      sourceDim = [
+        (buffer as HTMLVideoElement).videoWidth,
+        (buffer as HTMLVideoElement).videoHeight,
+      ];
+    else if (type === "image")
+      sourceDim = [
+        (buffer as HTMLImageElement).naturalWidth,
+        (buffer as HTMLImageElement).naturalHeight,
+      ];
 
     const mediaDim: [number, number] = [
       sourceDim[0] * Math.min(width / sourceDim[0], height / sourceDim[1]),
       sourceDim[1] * Math.min(width / sourceDim[0], height / sourceDim[1]),
     ];
 
-    const mediaPos: [number, number] = [Math.max(0, (width - mediaDim[0]) / 2), Math.max(0, (height - mediaDim[1]) / 2)];
+    const mediaPos: [number, number] = [
+      Math.max(0, (width - mediaDim[0]) / 2),
+      Math.max(0, (height - mediaDim[1]) / 2),
+    ];
 
     return { sourceDim, mediaDim, mediaPos };
   };
 
   let render: Render;
   $: render = ({ context, width, height }) => {
+    let totalPerf = performance.now();
     if (!$current.video || $current.video.length === 0) {
       context.fillStyle = "black";
       context.fillRect(0, 0, width, height);
@@ -34,9 +49,25 @@
       const clip = $timeline.clips.video[i].get(currentUUID);
       if (!clip || !clip.buffer) continue;
 
-      const { sourceDim, mediaDim, mediaPos } = calcDimensions(clip.type, clip.buffer);
-      context.drawImage(clip.buffer, 0, 0, ...sourceDim, ...mediaPos, ...mediaDim);
+      let perf = performance.now();
+      const { sourceDim, mediaDim, mediaPos } = calcDimensions(
+        clip.type,
+        clip.buffer
+      );
+      console.log(`calc: ${performance.now() - perf}`);
+
+      perf = performance.now();
+      context.drawImage(
+        clip.buffer,
+        0,
+        0,
+        ...sourceDim,
+        ...mediaPos,
+        ...mediaDim
+      );
+      console.log(`draw: ${performance.now() - perf}`);
     }
+    console.log(`total: ${performance.now() - totalPerf}`);
   };
 </script>
 
